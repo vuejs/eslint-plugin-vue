2262ac71dc75b4fdcd473e687a18a76a65c22af8
Rahul Kadyan <hi@znck.me>
2017-01-17 18:29:45 +0530

    Require eslint-plugin-html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict'
 
+require('eslint-plugin-html')
+
 module.exports = {
-  plugins: [
-    'html'
+  settings: [
+    "html/html-extensions": [".vue"],
+     "html/xml-extensions": []
   ],
   rules: {
     'jsx-uses-vars': require('eslint-plugin-react/lib/rules/jsx-uses-vars')
