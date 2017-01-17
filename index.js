2fade048ba06a6a5795868413433d2712ad8fcf1
Rahul Kadyan <hi@znck.me>
2017-01-17 18:13:49 +0530

    Upgrade version of eslint-plugin-html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = {
-  processors: {
-    '.vue': require('eslint-plugin-html').processors['.vue']
-  },
+  plugins: [
+    'html'
+  ],
   rules: {
     'jsx-uses-vars': require('eslint-plugin-react/lib/rules/jsx-uses-vars')
   }
