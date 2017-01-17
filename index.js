0967cb20c99d136788160dea2333bd2a42901cb8
Chris Fritz <chrisvfritz@gmail.com>
2017-01-17 08:27:59 -0500

    Update index.js from double quotes to single

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require('eslint-plugin-html')
 
 module.exports = {
   settings: [
-    "html/html-extensions": [".vue"],
-     "html/xml-extensions": []
+    'html/html-extensions': ['.vue'],
+    'html/xml-extensions': []
   ],
   rules: {
     'jsx-uses-vars': require('eslint-plugin-react/lib/rules/jsx-uses-vars')
