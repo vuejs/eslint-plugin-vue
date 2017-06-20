bbcf47fe2b6b0e09e94a535f104b42343f4b9b6d
Michał Sajnóg <msajnog93@gmail.com>
2017-06-21 00:39:39 +0200

    Improve order-in-components rule, add more test scenarios

--- tests/lib/rules/order-in-components.js
+++ tests/lib/rules/order-in-components.js
@@ -28,6 +28,51 @@ ruleTester.run('order-in-components', rule, {
         }
       `,
       parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    },
+    {
+      filename: 'test.jsx',
+      code: `
+        export default {
+          name: 'app',
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          },
+        }
+      `,
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    },
+    {
+      filename: 'test.js',
+      code: `
+        Vue.component('smart-list', {
+          name: 'app',
+          components: {},
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          }
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 }
+    },
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          name: 'app',
+          components: {},
+          el: '#app',
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          }
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 }
     }
   ],
 
@@ -51,6 +96,85 @@ ruleTester.run('order-in-components', rule, {
       errors: [{
         message: 'The "props" property should be above the "data" property on line 4.',
         line: 9
+      }]
+    },
+    {
+      filename: 'test.jsx',
+      code: `
+        export default {
+          render (h) {
+            return (
+              <span>{ this.msg }</span>
+            )
+          },
+          name: 'app',
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          },
+          props: {
+            propA: Number,
+          },
+        }
+      `,
+      parserOptions: { ecmaVersion: 6, sourceType: 'module', ecmaFeatures: { jsx: true }},
+      errors: [{
+        message: 'The "name" property should be above the "render" property on line 3.',
+        line: 8
+      }, {
+        message: 'The "props" property should be above the "data" property on line 9.',
+        line: 14
+      }]
+    },
+    {
+      filename: 'test.js',
+      code: `
+        Vue.component('smart-list', {
+          name: 'app',
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          },
+          components: {},
+          template: '<div></div>'
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: 'The "components" property should be above the "data" property on line 4.',
+        line: 9
+      }, {
+        message: 'The "template" property should be above the "data" property on line 4.',
+        line: 10
+      }]
+    },
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          el: '#app',
+          name: 'app',
+          data () {
+            return {
+              msg: 'Welcome to Your Vue.js App'
+            }
+          },
+          components: {},
+          template: '<div></div>'
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: 'The "name" property should be above the "el" property on line 3.',
+        line: 4
+      }, {
+        message: 'The "components" property should be above the "data" property on line 5.',
+        line: 10
+      }, {
+        message: 'The "template" property should be above the "data" property on line 5.',
+        line: 11
       }]
     }
   ]
