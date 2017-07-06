1d347132f120d6e5c085b8d3ee79170002068cb8
Filipa Lacerda <filipa@gitlab.com>
2017-07-06 23:30:36 +0100

    Adds rule to check for number of attributes per line

--- tests/lib/rules/max-attributes-per-line.js
+++ tests/lib/rules/max-attributes-per-line.js
@@ -2,36 +2,90 @@
  * @fileoverview Define the number of attributes allows per line
  * @author Filipa Lacerda
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/max-attributes-per-line"),
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/max-attributes-per-line')
 
-    RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("max-attributes-per-line", rule, {
+const ruleTester = new RuleTester({
+  parser: 'vue-eslint-parser',
+  parserOptions: { ecmaVersion: 2015 }
+})
 
-    valid: [
+ruleTester.run('max-attributes-per-line', rule, {
+  valid: [
+    {
+      code: `<template><component></component></template>`,
+      options: [{ singleline: 1, multiline: 1, firstline: 0 }]
+    },
+    {
+      code: `<template><component name="John Doe" age="30" job="Vet"></component></template>`,
+      options: [{ singleline: 3, multiline: 1, firstline: 0 }]
+    },
+    {
+      code: `<template><component name="John Doe"
+        age="30">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 1, firstline: 1 }]
+    },
+    {
+      code: `<template><component
+        name="John Doe"
+        age="30">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 1, firstline: 0 }]
+    },
+    {
+      code: `<template><component
+        name="John Doe" age="30"
+        job="Vet">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 2, firstline: 0 }]
+    }
+  ],
 
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: `<template><component name="John Doe" age="30" job="Vet"></component></template>`,
+      options: [{ singleline: 1, multiline: 1, firstline: 0 }],
+      errors: ['It has more than 1 attributes per line.']
+    },
+    {
+      code: `<template><component name="John Doe"
+        age="30">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 1, firstline: 0 }],
+      errors: ['Attribute name should be on a new line.']
+    },
+    {
+      code: `<template><component
+        name="John Doe" age="30"
+        job="Vet">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 1, firstline: 0 }],
+      errors: ['Attribute age should be on a new line.']
+    },
+    {
+      code: `<template><component
+        name="John Doe" age="30"
+        job="Vet" pet="dog" petname="Snoopy">
+        </component>
+      </template>`,
+      options: [{ singleline: 3, multiline: 2, firstline: 0 }],
+      errors: ['Attribute petname should be on a new line.']
+    }
+  ]
+})
