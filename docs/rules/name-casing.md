850297138880291b7b57512dbf4396e808e0973a
Armano <armano2@users.noreply.github.com>
2017-07-19 19:50:33 +0200

    Add `name-casing` rule.

---
+++ docs/rules/name-casing.md
@@ -0,0 +1,37 @@
+# Name property casing for consistency purposes (name-casing)
+
+Define a style for the `name` property casing for consistency purposes
+
+## :book: Rule Details
+
+:+1: Examples of **correct** code for `PascalCase`:
+
+```js
+export default {
+  name: 'MyComponent'
+}
+```
+
+:+1: Examples of **correct** code for `kebab-case`:
+
+```js
+export default {
+  name: 'my-component'
+}
+```
+
+:+1: Examples of **correct** code for `camelCase`:
+
+```js
+export default {
+  name: 'myComponent'
+}
+```
+
+## :wrench: Options
+
+Default casing is set to `PascalCase`
+
+```
+'vue/name-casing': [2, 'camelCase'|'kebab-case'|'PascalCase']
+```
---
+++ tests/lib/rules/name-casing.js
@@ -0,0 +1,125 @@
+/**
+ * @fileoverview Define a style for the name property casing for consistency purposes
+ * @author Armano
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/name-casing')
+const RuleTester = require('eslint').RuleTester
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester()
+ruleTester.run('name-casing', rule, {
+
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+        }
+      `,
+      options: ['camelCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'fooBar'
+        }
+      `,
+      options: ['camelCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'FooBar'
+        }
+      `,
+      options: ['PascalCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'foo-bar'
+        }
+      `,
+      options: ['kebab-case'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+    }
+  ],
+
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'foo-bar'
+        }
+      `,
+      options: ['camelCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      errors: [{
+        message: 'Property name "foo-bar" is not camelCase.',
+        type: 'Literal',
+        line: 3
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'foo  bar'
+        }
+      `,
+      options: ['PascalCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      errors: [{
+        message: 'Property name "foo  bar" is not PascalCase.',
+        type: 'Literal',
+        line: 3
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          name: 'foo!bar'
+        }
+      `,
+      options: ['camelCase'],
+      parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      errors: [{
+        message: 'Property name "foo!bar" is not camelCase.',
+        type: 'Literal',
+        line: 3
+      }]
+    },
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          name: 'foo!bar'
+        })
+      `,
+      options: ['camelCase'],
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: 'Property name "foo!bar" is not camelCase.',
+        type: 'Literal',
+        line: 3
+      }]
+    }
+  ]
+})
