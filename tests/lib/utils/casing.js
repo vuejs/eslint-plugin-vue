33613662a9e715d56e72579626d8e934c6628006
Armano <armano2@users.noreply.github.com>
2017-07-25 00:44:42 +0200

    Allow to use numbers in component names (#106)
    * Allow to use numbers in component names
    
    * Move casing to separate file and add unit tests

---
+++ tests/lib/utils/casing.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const casing = require('../../../lib/utils/casing')
+const chai = require('chai')
+
+const assert = chai.assert
+
+describe('getConverter()', () => {
+  it('should conver string to camelCase', () => {
+    const converter = casing.getConverter('camelCase')
+
+    assert.ok(converter('fooBar') === 'fooBar')
+    assert.ok(converter('foo-bar') === 'fooBar')
+    assert.ok(converter('FooBar') === 'fooBar')
+    assert.ok(converter('Foo1Bar') === 'foo1Bar')
+  })
+
+  it('should conver string to PascalCase', () => {
+    const converter = casing.getConverter('PascalCase')
+
+    assert.ok(converter('fooBar') === 'FooBar')
+    assert.ok(converter('foo-bar') === 'FooBar')
+    assert.ok(converter('FooBar') === 'FooBar')
+    assert.ok(converter('Foo1Bar') === 'Foo1Bar')
+  })
+
+  it('should conver string to kebab-case', () => {
+    const converter = casing.getConverter('kebab-case')
+
+    assert.ok(converter('fooBar') === 'foo-bar')
+    assert.ok(converter('foo-bar') === 'foo-bar')
+    assert.ok(converter('FooBar') === 'foo-bar')
+    assert.ok(converter('Foo1Bar') === 'foo1bar')
+  })
+})
