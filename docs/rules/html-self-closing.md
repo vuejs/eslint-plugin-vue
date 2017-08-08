0e46f31bbcd78fb07502daac40da028ac21ee4bc
Toru Nagashima <star.ctor@gmail.com>
2017-08-08 23:44:35 +0900

    New: `html-self-closing` rule (fixes #31) (#129)
    * New: `html-self-closing-style` rule (fixes #31)
    
    * add more tests.
    
    * Docs: add replacedBy property to html-no-self-closing
    
    * rename
    
    * update for review

---
+++ docs/rules/html-self-closing.md
@@ -0,0 +1,67 @@
+# Enforce self-closing style (html-self-closing)
+
+In Vue.js template, we can use either two styles for elements which don't have their content.
+
+1. `<your-component></your-component>`
+2. `<your-component />` (self-closing)
+
+Self-closing is simple and shorter, but it's not supported in raw HTML.
+This rule helps you to unify the self-closing style.
+
+## Rule Details
+
+This rule has options which specify self-closing style for each context.
+
+```json
+{
+    "html-self-closing": ["error", {
+        "html": {
+            "normal": "never",
+            "void": "never",
+            "component": "always"
+        },
+        "svg": "always",
+        "math": "always"
+    }]
+}
+```
+
+- `html.normal` (`"never"` by default) ... The style of well-known HTML elements except void elements.
+- `html.void` (`"never"` by default) ... The style of well-known HTML void elements.
+- `html.component` (`"always"` by default) ... The style of Vue.js custom components.
+- `svg`(`"always"` by default) .... The style of well-known SVG elements.
+- `math`(`"always"` by default) .... The style of well-known MathML elements.
+
+Every option can be set to one of the following values:
+
+- `"always"` ... Require self-closing at elements which don't have their content.
+- `"never"` ... Disallow self-closing.
+- `"any"` ... Don't enforce self-closing style.
+
+----
+
+:-1: Examples of **incorrect** code for this rule:
+
+```html
+/*eslint html-self-closing: "error"*/
+
+<template>
+    <div />
+    <img />
+    <your-component></your-component>
+    <svg><path d=""></path></svg>
+</template>
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```html
+/*eslint html-self-closing: "error"*/
+
+<template>
+    <div></div>
+    <img>
+    <your-component />
+    <svg><path d="" /></svg>
+</template>
+```
---
+++ tests/lib/rules/html-self-closing.js
@@ -0,0 +1,292 @@
+/**
+ * @author Toru Nagashima
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/html-self-closing')
+const RuleTester = require('eslint').RuleTester
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const tester = new RuleTester({
+  parser: 'vue-eslint-parser'
+})
+
+const ALL_CODE = `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`
+
+const anyWith = (opts) => Object.assign(
+  {
+    svg: 'any',
+    math: 'any'
+  },
+  opts,
+  {
+    html: Object.assign(
+      {
+        normal: 'any',
+        void: 'any',
+        component: 'any'
+      },
+      opts.html || {}
+    )
+  }
+)
+
+tester.run('html-self-closing', rule, {
+  valid: [
+    // default
+    '<template><div></div></template>',
+    '<template><img></template>',
+    '<template><x-test/></template>',
+    '<template><svg><path/></svg></template>',
+    '<template><math><mspace/></math></template>',
+
+    // Don't error if there are comments in their content.
+    {
+      code: '<template><div><!-- comment --></div></template>',
+      output: null,
+      options: [{ html: { normal: 'always' }}]
+    }
+
+    // other cases are in `invalid` tests.
+  ],
+  invalid: [
+    // default
+    {
+      code: '<template><div/></template>',
+      output: '<template><div></div></template>',
+      errors: ['Disallow self-closing on HTML elements (<div/>).']
+    },
+    {
+      code: '<template><img/></template>',
+      output: '<template><img></template>',
+      errors: ['Disallow self-closing on HTML void elements (<img/>).']
+    },
+    {
+      code: '<template><x-test></x-test></template>',
+      output: '<template><x-test/></template>',
+      errors: ['Require self-closing on Vue.js custom components (<x-test>).']
+    },
+    {
+      code: '<template><svg><path></path></svg></template>',
+      output: '<template><svg><path/></svg></template>',
+      errors: ['Require self-closing on SVG elements (<path>).']
+    },
+    {
+      code: '<template><math><mspace></mspace></math></template>',
+      output: '<template><math><mspace/></math></template>',
+      errors: ['Require self-closing on MathML elements (<mspace>).']
+    },
+
+    // others
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div/>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { normal: 'always' }})],
+      errors: [
+        { message: 'Require self-closing on HTML elements (<div>).', line: 2 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div></div>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { normal: 'never' }})],
+      errors: [
+        { message: 'Disallow self-closing on HTML elements (<div/>).', line: 3 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img/>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { void: 'always' }})],
+      errors: [
+        { message: 'Require self-closing on HTML void elements (<img>).', line: 4 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { void: 'never' }})],
+      errors: [
+        { message: 'Disallow self-closing on HTML void elements (<img/>).', line: 5 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test/>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { component: 'always' }})],
+      errors: [
+        { message: 'Require self-closing on Vue.js custom components (<x-test>).', line: 6 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test></x-test>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ html: { component: 'never' }})],
+      errors: [
+        { message: 'Disallow self-closing on Vue.js custom components (<x-test/>).', line: 7 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path/></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ svg: 'always' })],
+      errors: [
+        { message: 'Require self-closing on SVG elements (<path>).', line: 8 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path></path></svg>
+  <math><mspace></mspace></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ svg: 'never' })],
+      errors: [
+        { message: 'Disallow self-closing on SVG elements (<path/>).', line: 9 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace/></math>
+  <math><mspace/></math>
+</template>`,
+      options: [anyWith({ math: 'always' })],
+      errors: [
+        { message: 'Require self-closing on MathML elements (<mspace>).', line: 10 }
+      ]
+    },
+    {
+      code: ALL_CODE,
+      output: `<template>
+  <div></div>
+  <div/>
+  <img>
+  <img/>
+  <x-test></x-test>
+  <x-test/>
+  <svg><path></path></svg>
+  <svg><path/></svg>
+  <math><mspace></mspace></math>
+  <math><mspace></mspace></math>
+</template>`,
+      options: [anyWith({ math: 'never' })],
+      errors: [
+        { message: 'Disallow self-closing on MathML elements (<mspace/>).', line: 11 }
+      ]
+    }
+  ]
+})
