2d15606c761d8cac7192a6aaa37d6ddf0b2dcfac
Armano <armano2@users.noreply.github.com>
2017-08-14 23:50:31 +0200

    Add rule `no-template-shadow`. fixes #101

---
+++ tests/lib/rules/no-template-shadow.js
@@ -0,0 +1,127 @@
+/**
+ * @fileoverview Disallow variable declarations from shadowing variables declared in the outer scope.
+ * @author Armano
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-template-shadow')
+const RuleTester = require('eslint').RuleTester
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module'
+  }
+})
+
+ruleTester.run('no-template-shadow', rule, {
+
+  valid: [
+    '',
+    '<template><div></div></template>',
+    '<template><div v-for="i in 5"></div></template>',
+    '<template><div v-for="i in 5"><div v-for="b in 5"></div></div></template>',
+    '<template><div v-for="i in 5"></div><div v-for="i in 5"></div></template>',
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in 5"></div>
+        <div v-for="i in 5"></div>
+      </template>
+      <script>
+        export default {
+          computed: {
+            f () { }
+          }
+        }
+      </script>`
+    }
+  ],
+
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: '<template><div v-for="i in 5"><div v-for="i in 5"></div></div></template>',
+      errors: [{
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in 5">
+          <div v-for="i in 5"></div>
+        </div>
+      </template>
+      <script>
+        export default {
+          data: {
+            i: 7
+          }
+        }
+      </script>`,
+      errors: [{
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }, {
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in 5"></div>
+        <div v-for="i in 5"></div>
+      </template>
+      <script>
+        export default {
+          data: {
+            i: 7
+          }
+        }
+      </script>`,
+      errors: [{
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }, {
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in 5"></div>
+        <div v-for="f in 5"></div>
+      </template>
+      <script>
+        export default {
+          computed: {
+            i () { }
+          },
+          methods: {
+            f () { }
+          }
+        }
+      </script>`,
+      errors: [{
+        message: "Variable 'i' is already declared in the upper scope.",
+        type: 'Identifier'
+      }, {
+        message: "Variable 'f' is already declared in the upper scope.",
+        type: 'Identifier'
+      }]
+    }
+  ]
+})
