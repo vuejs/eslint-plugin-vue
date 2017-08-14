a39a460109bb6f431445ba5a436555f6b08d87a9
Armano <armano2@users.noreply.github.com>
2017-08-14 13:35:30 +0200

    Rename rule `no-reservered-keys` to `no-reserved-keys`. fixes #155

---
+++ docs/rules/no-reserved-keys.md
@@ -0,0 +1,58 @@
+# Prevent overwrite reserved keys (no-reserved-keys)
+
+This rule prevents to use reserved names from to avoid conflicts and unexpected behavior.
+
+## Rule Details
+
+:-1: Examples of **incorrect** code for this rule:
+
+```js
+export default {
+  props: {
+    $el: String
+  },
+  computed: {
+    $on: {
+      get () {
+      }
+    }
+  },
+  data: {
+    _foo: null
+  },
+  methods: {
+    $nextTick () {
+    }
+  }
+}
+```
+
+## :wrench: Options
+
+This rule has an object option:
+
+`"reserved"`: [] (default) array of dissalowed names inside `groups`.
+
+`"groups"`: [] (default) array of additional groups to search for duplicates.
+
+### Example:
+
+```
+vue/no-reserved-keys: [2, {
+  reserved: ['foo', 'foo2'],
+  groups: ['asyncComputed']
+}]
+```
+
+:-1: Examples of **incorrect** code for this configuration
+
+```js
+export default {
+  asyncComputed: {
+    foo2 () {}
+  },
+  computed: {
+    foo () {}
+  }
+}
+```
---
+++ tests/lib/rules/no-reserved-keys.js
@@ -0,0 +1,100 @@
+/**
+ * @fileoverview Prevent overwrite reserved keys
+ * @author Armano
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-reserved-keys')
+const RuleTester = require('eslint').RuleTester
+
+const parserOptions = {
+  ecmaVersion: 7,
+  sourceType: 'module',
+  ecmaFeatures: { experimentalObjectRestSpread: true }
+}
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-reserved-keys', rule, {
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          props: ['foo'],
+          computed: {
+            bar () {
+            }
+          },
+          data () {
+            return {
+              dat: null
+            }
+          },
+          methods: {
+            _foo () {},
+            test () {
+            }
+          }
+        }
+      `,
+      parserOptions
+    }
+  ],
+
+  invalid: [
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          props: {
+            $el: String
+          }
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: "Key '$el' is reserved.",
+        line: 4
+      }]
+    },
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          data: {
+            _foo: String
+          }
+        })
+      `,
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: "Keys starting with with '_' are reserved in '_foo' group.",
+        line: 4
+      }]
+    },
+    {
+      filename: 'test.js',
+      code: `
+        new Vue({
+          foo: {
+            bar: String
+          }
+        })
+      `,
+      options: [{ reserved: ['bar'], groups: ['foo'] }],
+      parserOptions: { ecmaVersion: 6 },
+      errors: [{
+        message: "Key 'bar' is reserved.",
+        line: 4
+      }]
+    }
+  ]
+})
