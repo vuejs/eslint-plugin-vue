71929fa3b42094ed083cb850e15dd292832d51d3
Michał Sajnóg <msajnog93@gmail.com>
2017-08-21 11:25:52 +0200

    Add "require-default-prop" rule (fixes #122) (#135)
    * Add "require-default-prop" rule
    
    * Handle spread operator cases

---
+++ docs/rules/require-default-prop.md
@@ -0,0 +1,45 @@
+# Require default value for props (require-default-prop)
+
+This rule requires default value to be set for each props that are not marked as `required`.
+
+## Rule Details
+
+Examples of **incorrect** code for this rule:
+
+```js
+export default {
+  props: {
+    a: Number,
+    b: [Number, String],
+    c: {
+      type: Number
+    },
+    d: {
+      type: Number,
+      required: false
+    }
+  }
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+export default {
+  props: {
+    a: {
+      type: Number,
+      required: true
+    },
+    b: {
+      type: Number,
+      default: 0
+    },
+    c: {
+      type: Number,
+      default: 0,
+      required: false
+    }
+  }
+}
+```
---
+++ tests/lib/rules/require-default-prop.js
@@ -0,0 +1,108 @@
+/**
+ * @fileoverview Require default value for props
+ * @author Michał Sajnóg <msajnog93@gmail.com> (http://github.com/michalsnik)
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/require-default-prop')
+const RuleTester = require('eslint').RuleTester
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: { experimentalObjectRestSpread: true },
+  sourceType: 'module'
+}
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester()
+ruleTester.run('require-default-prop', rule, {
+
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          props: {
+            a: {
+              type: Number,
+              required: true
+            },
+            b: {
+              type: Number,
+              default: 0
+            },
+            c: {
+              type: Number,
+              default: 0,
+              required: false
+            },
+            // eslint-disable-next-line require-default-prop
+            d: Number
+          }
+        }
+      `,
+      parserOptions
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          props: {
+            ...x,
+            a: {
+              ...y,
+              type: Number,
+              required: true
+            },
+            b: {
+              type: Number,
+              default: 0
+            }
+          }
+        }
+      `,
+      parserOptions
+    }
+  ],
+
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          props: {
+            a: Number,
+            b: [Number, String],
+            c: {
+              type: Number
+            },
+            d: {
+              type: Number,
+              required: false
+            }
+          }
+        }
+      `,
+      parserOptions,
+      errors: [{
+        message: `Prop 'a' requires default value to be set.`,
+        line: 4
+      }, {
+        message: `Prop 'b' requires default value to be set.`,
+        line: 5
+      }, {
+        message: `Prop 'c' requires default value to be set.`,
+        line: 6
+      }, {
+        message: `Prop 'd' requires default value to be set.`,
+        line: 9
+      }]
+    }
+  ]
+})
