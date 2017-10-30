43c3f3ad08ee421ec5ef70aff6faeed1e0415056
薛定谔的猫 <hh_2013@foxmail.com>
2017-10-30 11:52:19 +0800

    New: no-unused-vars (fixes #100)(#187)

---
+++ tests/lib/rules/no-unused-vars.js
@@ -0,0 +1,67 @@
+/**
+ * @author 薛定谔的猫<hh_2013@foxmail.com>
+ * @copyright 2017 薛定谔的猫. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/no-unused-vars')
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const tester = new RuleTester({
+  parser: 'vue-eslint-parser',
+  parserOptions: { ecmaVersion: 2015 }
+})
+
+tester.run('no-unused-vars', rule, {
+  valid: [
+    {
+      code: '<template><ol v-for="i in 5"><li>{{i}}</li></ol></template>'
+    },
+    {
+      code: '<template><ol v-for="i in 5"><li :prop="i"></li></ol></template>'
+    },
+    {
+      code: '<template v-for="i in 5"><comp v-for="j in 10">{{i}}{{j}}</comp></template>'
+    },
+    {
+      code: '<template><ol v-for="i in data"><li v-for="f in i">{{ f.bar.baz }}</li></ol></template>'
+    },
+    {
+      code: '<template scope="props">{{props}}</template>'
+    },
+    {
+      code: '<template scope="props"><span v-if="props"></span></template>'
+    }
+  ],
+  invalid: [
+    {
+      code: '<template><ol v-for="i in 5"><li></li></ol></template>',
+      errors: ['\'i\' is defined but never used.']
+    },
+    {
+      code: '<template scope="props"></template>',
+      errors: ['\'props\' is defined but never used.']
+    },
+    {
+      code: '<template v-for="i in 5"><comp v-for="j in 10">{{i}}{{i}}</comp></template>',
+      errors: ['\'j\' is defined but never used.']
+    },
+    {
+      code: '<template><ol v-for="i in data"><li v-for="f in i"></li></ol></template>',
+      errors: ['\'f\' is defined but never used.']
+    },
+    {
+      code: '<template><div v-for="(v, i, c) in foo"></div></template>',
+      errors: ['\'v\' is defined but never used.', '\'i\' is defined but never used.', '\'c\' is defined but never used.']
+    }
+  ]
+})
