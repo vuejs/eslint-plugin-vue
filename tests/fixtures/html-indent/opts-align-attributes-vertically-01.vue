c423ee604f42e1de5a15895f5ba944e1035fdc62
Toru Nagashima <star.ctor@gmail.com>
2017-12-31 14:20:47 +0900

    Update: add alignAttributesVertically

---
+++ tests/fixtures/html-indent/opts-align-attributes-vertically-01.vue
@@ -0,0 +1,14 @@
+<!--{"options":[2,{"alignAttributesVertically":false}]}-->
+<template>
+  <div
+    a="a"
+    b="b"
+    c=
+      "c"
+    d
+      ="d"
+    e
+    f
+      =
+  ></div>
+</template>
