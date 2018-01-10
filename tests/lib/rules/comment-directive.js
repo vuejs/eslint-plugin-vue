c4356d5b2c15093dc8d826698ac0da7e28e517ce
Toru Nagashima <star.ctor@gmail.com>
2018-01-11 07:04:55 +0900

    Fix: unintentional disabling (#338)

--- tests/lib/rules/comment-directive.js
+++ tests/lib/rules/comment-directive.js
@@ -26,6 +26,7 @@ const linter = new eslint.CLIEngine({
   },
   plugins: ['vue'],
   rules: {
+    'no-unused-vars': 'error',
     'vue/comment-directive': 'error',
     'vue/no-parsing-error': 'error',
     'vue/no-duplicate-attributes': 'error'
@@ -107,6 +108,22 @@ describe('comment-directive', () => {
       assert.deepEqual(messages.length, 1)
       assert.deepEqual(messages[0].ruleId, 'vue/no-duplicate-attributes')
       assert.deepEqual(messages[0].line, 6)
+    })
+
+    it('should not affect to the code in <script>.', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable -->
+          <div id id="a">Hello</div>
+        </template>
+        <script>
+          var a
+        </script>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0].messages
+
+      assert.strictEqual(messages.length, 1)
+      assert.strictEqual(messages[0].ruleId, 'no-unused-vars')
     })
   })
 
