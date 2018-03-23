670970252f242c5629a9ea059040f5d2e94a9a7a
yosuke ota <otameshiyo23@gmail.com>
2018-03-24 01:34:22 +0900

    [fixed] error message

--- tests/lib/rules/no-use-v-if-with-v-for.js
+++ tests/lib/rules/no-use-v-if-with-v-for.js
@@ -79,17 +79,26 @@ tester.run('no-use-v-if-with-v-for', rule, {
     {
       filename: 'test.vue',
       code: '<template><div><div v-for="x in list" v-if="shown"></div></div></template>',
-      errors: ["This 'v-if' should be moved to the wrapper element."]
+      errors: [{
+        message: "This 'v-if' should be moved to the wrapper element.",
+        line: 1
+      }]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div v-for="x in list" v-if="list.length&gt;0"></div></div></template>',
-      errors: ["This 'v-if' should be moved to the wrapper element."]
+      errors: [{
+        message: "This 'v-if' should be moved to the wrapper element.",
+        line: 1
+      }]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div v-for="x in list" v-if="x.isActive"></div></div></template>',
-      errors: ["The 'v-for' list variable should be replace with a new computed property that returns your filtered list by this 'v-if' condition."]
+      errors: [{
+        message: "The 'list' variable inside 'v-for' directive should be replaced with a computed property that returns filtered array instead. You should not mix 'v-for' with 'v-if'.",
+        line: 1
+      }]
     },
     {
       filename: 'test.vue',
@@ -106,7 +115,10 @@ tester.run('no-use-v-if-with-v-for', rule, {
           </ul>
         </template>
       `,
-      errors: ["The 'v-for' list variable should be replace with a new computed property that returns your filtered list by this 'v-if' condition."]
+      errors: [{
+        message: "The 'users' variable inside 'v-for' directive should be replaced with a computed property that returns filtered array instead. You should not mix 'v-for' with 'v-if'.",
+        line: 6
+      }]
     },
     {
       filename: 'test.vue',
@@ -123,7 +135,10 @@ tester.run('no-use-v-if-with-v-for', rule, {
           </ul>
         </template>
       `,
-      errors: ["This 'v-if' should be moved to the wrapper element."]
+      errors: [{
+        message: "This 'v-if' should be moved to the wrapper element.",
+        line: 6
+      }]
     }
   ]
 })
