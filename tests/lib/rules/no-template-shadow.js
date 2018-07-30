2a3a92b6823fd6beb569f245e27189405ca6330c
Michał Sajnóg <msajnog93@gmail.com>
2018-07-30 12:42:42 +0200

    Add more test cases

--- tests/lib/rules/no-template-shadow.js
+++ tests/lib/rules/no-template-shadow.js
@@ -18,7 +18,7 @@ const RuleTester = require('eslint').RuleTester
 const ruleTester = new RuleTester({
   parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 2015,
+    ecmaVersion: 2018,
     sourceType: 'module'
   }
 })
@@ -43,6 +43,30 @@ ruleTester.run('no-template-shadow', rule, {
         export default {
           computed: {
             f () { }
+          }
+        }
+      </script>`
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in b" />
+        <div v-for="b in c" />
+        <div v-for="d in f" />
+      </template>
+      <script>
+        export default {
+          ...a,
+          data() {
+            return {
+              ...b,
+              c: [1, 2, 3]
+            }
+          },
+          computed: {
+            ...d,
+            e,
+            ['f']: [1, 2],
           }
         }
       </script>`
@@ -147,6 +171,42 @@ ruleTester.run('no-template-shadow', rule, {
         message: "Variable 'f' is already declared in the upper scope.",
         type: 'Identifier',
         line: 3
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div v-for="i in c" />
+        <div v-for="a in c" />
+        <div v-for="b in c" />
+        <div v-for="d in c" />
+        <div v-for="e in f" />
+        <div v-for="f in c" />
+      </template>
+      <script>
+        export default {
+          ...a,
+          data() {
+            return {
+              ...b,
+              c: [1, 2, 3]
+            }
+          },
+          computed: {
+            ...d,
+            e,
+            ['f']: [1, 2],
+          }
+        }
+      </script>`,
+      errors: [{
+        message: "Variable 'e' is already declared in the upper scope.",
+        type: 'Identifier',
+        line: 6
+      }, {
+        message: "Variable 'f' is already declared in the upper scope.",
+        type: 'Identifier',
+        line: 7
       }]
     }
   ]
