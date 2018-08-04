5a83e46f87cedd7758a997032b37103e992873eb
Toru Nagashima <star.ctor@gmail.com>
2018-08-04 11:44:26 +0900

    update eslint-code-block component

--- docs/.vuepress/components/eslint-code-block.vue
+++ docs/.vuepress/components/eslint-code-block.vue
@@ -2,7 +2,7 @@
     <eslint-editor
         :linter="linter"
         :config="config"
-        :code="cookedCode"
+        :code="code"
         :style="{ height }"
         class="eslint-code-block"
         filename="example.vue"
@@ -22,10 +22,6 @@ export default {
   components: { EslintEditor },
 
   props: {
-    code: {
-      type: String,
-      default: ''
-    },
     rules: {
       type: Object,
       default () {
@@ -77,13 +73,24 @@ export default {
       }
     },
 
-    cookedCode () {
-      return `${this.code.trim()}\n`
+    code () {
+      return `${this.computeCodeFromSlot(this.$slots.default).trim()}\n`
     },
 
     height () {
-      const lines = this.cookedCode.split('\n').length
+      const lines = this.code.split('\n').length
       return `${Math.max(120, 20 * (1 + lines))}px`
+    }
+  },
+
+  methods: {
+    computeCodeFromSlot (nodes) {
+      if (!Array.isArray(nodes)) {
+        return ''
+      }
+      return nodes.map(node =>
+        node.text || this.computeCodeFromSlot(node.children)
+      ).join('')
     }
   },
 
