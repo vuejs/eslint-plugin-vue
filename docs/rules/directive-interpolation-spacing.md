fdad618c94a4ec71ee580661cabd761174660e90
milewski <rafael.milewski@gmail.com>
2018-10-07 19:23:31 +0800

    add directive-interpolation-spacing rule

---
+++ docs/rules/directive-interpolation-spacing.md
@@ -0,0 +1,61 @@
+# enforce unified spacing in mustache interpolations within directive expressions (vue/directive-interpolation-spacing)
+
+- :gear: This rule is included in `"plugin:vue/strongly-recommended"` and `"plugin:vue/recommended"`.
+- :wrench: The `--fix` option on the [command line](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) can automatically fix some of the problems reported by this rule.
+
+## :book: Rule Details
+
+This rule aims to enforce unified spacing in directive interpolations.
+
+:-1: Examples of **incorrect** code for this rule:
+
+```html
+<div :property="{key:value}"></div>
+<div :property="{    key:value    }"></div>
+<div :property=" { key:value } "></div>
+<div :property="{ [expression]:value,[expression]:value }"></div>
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```html
+<div :property="{ key: value }"></div>
+<div :property="{ [expression]: value }"></div>
+<div :property="{ [expression]: value, [expression]: value }"></div>
+```
+
+## :wrench: Options
+
+Default spacing is set to `always`
+
+```
+'vue/directive-interpolation-spacing': [2, 'always'|'never']
+```
+
+### `"always"` - Expect one space between expression and curly braces.
+
+:-1: Examples of **incorrect** code for this rule:
+
+```html
+<div :class="{key:value}"></div>
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```html
+<div :class="{ key: value }"></div>
+```
+
+### `"never"` - Expect no spaces between expression and curly braces.
+
+:-1: Examples of **incorrect** code for this rule:
+
+```html
+<div :class="{ key: value }"></div>
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```html
+<div :class="{key: value}"></div>
+```
---
+++ tests/lib/rules/directive-interpolation-spacing.js
@@ -0,0 +1,149 @@
+/**
+ * @fileoverview enforce unified spacing in directive interpolations.
+ * @author Rafael Milewski <https://github.com/milewski>
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/directive-interpolation-spacing')
+const RuleTester = require('eslint').RuleTester
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: 'vue-eslint-parser',
+  parserOptions: { ecmaVersion: 2015 }
+})
+
+ruleTester.run('directive-interpolation-spacing', rule, {
+
+  valid: [
+    `<template><div :class="test"></div></template>`,
+    `<template><div :class="test && { a: true }"></div></template>`,
+    '<template><div :class="{ a: 123 }"></div></template>',
+    '<template><div :class="{ [a]: 123 }"></div></template>',
+    '<template><div :class="{ 1: 123 }"></div></template>',
+    '<template><div :class="{ test && { a: 123 } }"></div></template>',
+    '<template><div :class="{ test && { a: 123 } && test }"></div></template>',
+    `<template><div :class="{ a: 123 }" :styles="{ backgroundColor: 'red' }"></div></template>`,
+    `<template><div :class="{ a: 'a', b: 'b' }"></div></template>`,
+    `<template><div :class="{ [ true ? 'a' : 'b' ]: 123 }"></div></template>`,
+    `<template><div :class="{ [ false ? 'a' : true ? 'c' : 'b' ]: 123 }"></div></template>`,
+    {
+      code: `<template><div :class="{a: 'a', b: 'b'}"></div></template>`,
+      options: ['never']
+    }
+  ],
+
+  invalid: [
+    {
+      code: `<template><div :class="{ a: 'a', b: 'b'}"></div></template>`,
+      output: `<template><div :class="{ a: 'a', b: 'b' }"></div></template>`,
+      options: ['always'],
+      errors: [
+        `Expected 1 space before '}', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="{a: 123}"></div></template>',
+      output: '<template><div :class="{ a: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected 1 space after '{', but not found.`,
+        `Expected 1 space before '}', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="   { a: 123 }   "></div></template>',
+      output: '<template><div :class="{ a: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected no space before '{', but found.`,
+        `Expected no space after '}', but found.`
+      ]
+    },
+    {
+      code: '<template><div :class="{ a: 123 , b:123 }"></div></template>',
+      output: '<template><div :class="{ a: 123, b: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected no space before ',', but found.`,
+        `Expected 1 space after ':', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="{ a: 123,b: 123 }"></div></template>',
+      output: '<template><div :class="{ a: 123, b: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected 1 space after ',', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="{ [true?1:2]: 123 }"></div></template>',
+      output: '<template><div :class="{ [true ? 1: 2]: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected 1 space before '?', but not found.`,
+        `Expected 1 space after '?', but not found.`,
+        `Expected 1 space after ':', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="test &&{ test: 123 }"></div></template>',
+      output: '<template><div :class="test && { test: 123 }"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected 1 space before '{', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="test &&{ test: 123 }&& test"></div></template>',
+      output: '<template><div :class="test && { test: 123 } && test"></div></template>',
+      options: ['always'],
+      errors: [
+        `Expected 1 space before '{', but not found.`,
+        `Expected 1 space after '}', but not found.`
+      ]
+    },
+
+    /**
+     * Options: never
+     */
+    {
+      code: '<template><div :class="{ a: 123, b: 123 }"></div></template>',
+      output: '<template><div :class="{a: 123, b: 123}"></div></template>',
+      options: ['never'],
+      errors: [
+        `Expected no space after '{', but found.`,
+        `Expected no space before '}', but found.`
+      ]
+    },
+    {
+      code: '<template><div :class="{a:123,b:123}"></div></template>',
+      output: '<template><div :class="{a: 123, b: 123}"></div></template>',
+      options: ['never'],
+      errors: [
+        `Expected 1 space after ':', but not found.`,
+        `Expected 1 space after ',', but not found.`,
+        `Expected 1 space after ':', but not found.`
+      ]
+    },
+    {
+      code: '<template><div :class="test &&{ test: 123 }&& test"></div></template>',
+      output: '<template><div :class="test && {test: 123} && test"></div></template>',
+      options: ['never'],
+      errors: [
+        `Expected 1 space before '{', but not found.`,
+        `Expected no space after '{', but found.`,
+        `Expected no space before '}', but found.`,
+        `Expected 1 space after '}', but not found.`
+      ]
+    }
+  ]
+})
