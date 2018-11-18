2ac62e675d80d0022bdac132012ad0b248a4d879
Armano <armano2@users.noreply.github.com>
2018-11-18 15:12:22 +0100

    chore: enable no-mixed-operators to catch potentially harmful expressions

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
   rules: {
     'eslint-plugin/report-message-format': ['error', '^[A-Z`\'{].*\\.$'],
     'eslint-plugin/prefer-placeholders': 'error',
-    'eslint-plugin/consistent-output': 'error'
+    'eslint-plugin/consistent-output': 'error',
+    'no-mixed-operators': 'error'
   },
 
   overrides: [{
