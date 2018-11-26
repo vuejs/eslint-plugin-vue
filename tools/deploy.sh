ac990c2385288d57eee00502b0cac0b73134cbe1
Michał Sajnóg <msajnog93@gmail.com>
2018-11-26 15:07:27 +0100

    Uncomment deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,11 +2,11 @@
 
 set -e
 npm run docs:build
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
-# git init
-# git add -A
-# git commit -m 'deploy'
-# git push -f git@github.com:vuejs/eslint-plugin-vue.git master:gh-pages
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:vuejs/eslint-plugin-vue.git master:gh-pages
 
 cd -
