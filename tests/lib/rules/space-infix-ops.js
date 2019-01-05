226545d772430f052be6e68acf7dac2939f697b0
Toru Nagashima <star.ctor@gmail.com>
2019-01-05 17:23:06 +0900

    switch error messages for minimal version tests

--- tests/lib/rules/space-infix-ops.js
+++ tests/lib/rules/space-infix-ops.js
@@ -3,13 +3,18 @@
  */
 'use strict'
 
-const RuleTester = require('eslint').RuleTester
+const { RuleTester, CLIEngine } = require('eslint')
+const semver = require('semver')
 const rule = require('../../../lib/rules/space-infix-ops')
 
 const tester = new RuleTester({
   parser: 'vue-eslint-parser',
   parserOptions: { ecmaVersion: 2015 }
 })
+
+const message = semver.lt(CLIEngine.version, '5.10.0')
+  ? () => 'Infix operators must be spaced.'
+  : operator => `Operator '${operator}' must be spaced.`
 
 tester.run('space-infix-ops', rule, {
   valid: [
@@ -19,15 +24,15 @@ tester.run('space-infix-ops', rule, {
   invalid: [
     {
       code: '<template><div :attr="a+1" /></template>',
-      errors: ['Operator \'+\' must be spaced.']
+      errors: [message('+')]
     },
     {
       code: '<template><div :attr="a?1 : 2" /></template>',
-      errors: ['Operator \'?\' must be spaced.']
+      errors: [message('?')]
     },
     {
       code: '<template><div :attr="a ? 1:2" /></template>',
-      errors: ['Operator \':\' must be spaced.']
+      errors: [message(':')]
     }
   ]
 })
