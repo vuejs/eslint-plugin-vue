5e37ee393c951ad3c09caee0d45afc425f2b9b7d
yosuke ota <otameshiyo23@gmail.com>
2019-12-27 10:18:43 +0900

    Fixed wrong indentation

---
+++ tests/fixtures/html-indent/logical-expression-01.vue
@@ -0,0 +1,64 @@
+<!--{}-->
+<template>
+  <div>
+    <span
+      v-if="
+        a &&
+          b"
+    >
+    </span>
+    <span
+      v-if="
+        a
+          && b"
+    >
+    </span>
+    <span
+      v-if="
+        a
+          &&
+          b"
+    >
+    </span>
+    <!-- Enclosed in parentheses -->
+    <span
+      v-if="(
+        a &&
+        b
+      )"
+    >
+    </span>
+    <span
+      v-if="(
+        a
+        && b
+      )"
+    >
+    </span>
+    <span
+      v-if="(
+        a
+        &&
+        b
+      )"
+    >
+    </span>
+    <!-- The first line is the same line -->
+    <span
+      v-if="a &&
+        b"
+    >
+    </span>
+    <span
+      v-if="a
+        && b"
+    >
+    </span>
+    <span
+      v-if="a
+        &&
+        b"
+    >
+    </span>
+  </div>
+</template>
