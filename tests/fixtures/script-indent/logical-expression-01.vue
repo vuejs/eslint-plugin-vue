c8cdd77a0a3d7004b3137af789a5a7e9baefaae9
Yosuke Ota <otameshiyo23@gmail.com>
2019-12-27 15:18:56 +0900

    Fixed wrong indentation (#1015)

---
+++ tests/fixtures/script-indent/logical-expression-01.vue
@@ -0,0 +1,16 @@
+<!--{}-->
+<script>
+// https://github.com/vuejs/eslint-plugin-vue/issues/1007
+function fn () {
+  return (
+    (
+      a ||
+      b
+    ) &&
+    (
+      c ||
+      d
+    )
+  );
+}
+</script>
