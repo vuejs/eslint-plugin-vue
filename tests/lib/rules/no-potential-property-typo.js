b39cff983173489c3b46d0912446f8167e2aecbd
IWANABETHATGUY <974153916@qq.com>
2020-03-10 14:55:40 +0800

    feat: add test, add preset, custom option

--- tests/lib/rules/no-potential-property-typo.js
+++ tests/lib/rules/no-potential-property-typo.js
@@ -8,22 +8,74 @@
 // Requirements
 // ------------------------------------------------------------------------------
 
-var rule = require('../../../lib/rules/no-potential-property-typo')
+const rule = require('../../../lib/rules/no-potential-property-typo')
 
-var RuleTester = require('eslint').RuleTester
+const RuleTester = require('eslint').RuleTester
 
 // ------------------------------------------------------------------------------
 // Tests
 // ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester()
-ruleTester.run('no-potential-property-typo', rule, {
-
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: { ecmaVersion: 2017, sourceType: 'module' }
+})
+tester.run('no-potential-property-typo', rule, {
   valid: [
-
+    {
+      filename: 'test.vue',
+      code: `
+      export default {
+        dat: {},
+        method: {}
+      }
+      `
+    }
     // give me some code that won't trigger a warning
   ],
 
   invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+      export default {
+        dat: {},
+        method: {}
+      }
+      `,
+      options: [{ custom: ['data', 'methods'] }],
+      errors: [
+        `'dat' may be a typo, which is similar to vue component option 'data'.`,
+        `'method' may be a typo, which is similar to vue component option 'methods'.`
+        // {
+        //   message: `'dat' may be a typo, which is similar to vue component option 'data'.`
+        //   // suggestions: [
+        //   //   {
+        //   //     desc: `Replace property 'dat' to 'data'`,
+        //   //     output: `
+        //   //     export default {
+        //   //       data: {},
+        //   //       method: {}
+        //   //     }
+        //   //     `
+        //   //   }
+        //   // ]
+        // },
+        // {
+        //   message: `'method' may be a typo, which is similar to vue component option 'methods'.`
+        //   // suggestions: [
+        //   //   {
+        //   //     desc: `Replace property 'method' to 'methods'`,
+        //   //     output: `
+        //   //     export default {
+        //   //       dat: {},
+        //   //       methods: {}
+        //   //     }
+        //   //     `
+        //   //   }
+        //   // ]
+        // }
+      ]
+    }
   ]
 })
