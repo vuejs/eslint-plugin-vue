6d7d8508a52eefc597c681f3ca5167512faf70e2
IWANABETHATGUY <974153916@qq.com>
2020-03-12 00:14:47 +0800

    test: add test

--- tests/lib/rules/no-computed-in-data.js
+++ tests/lib/rules/no-computed-in-data.js
@@ -16,14 +16,96 @@ var RuleTester = require('eslint').RuleTester
 // Tests
 // ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester()
+var ruleTester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  }
+})
 ruleTester.run('no-computed-in-data', rule, {
 
   valid: [
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            a: this.b
+          }
+        },
+        computed: {
+          test() {},
+          'foo': {
 
+          }
+        }
+      }
+      </script>
+      `
+    }
     // give me some code that won't trigger a warning
   ],
 
   invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            a: this.test
+          }
+        },
+        computed: {
+          test() {},
+          'foo': {
+
+          }
+        }
+      }
+      </script>
+      `,
+      errors: [
+        `The "this.test" is an computed data can't use in data property.`
+      ]
+    },
+    // same computed data reference by multi data property
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            a: this.test,
+            b: this.test
+          }
+        },
+        computed: {
+          test() {},
+          'foo': {
+
+          }
+        }
+      }
+      </script>
+      `,
+      errors: [
+        {
+          message: `The "this.test" is an computed data can't use in data property.`,
+          line: 6,
+          column: 16
+        },
+        {
+          message: `The "this.test" is an computed data can't use in data property.`,
+          line: 7,
+          column: 16
+        }
+      ]
+    }
   ]
 })
