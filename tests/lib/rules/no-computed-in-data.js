7db773fbcacba85b389391b411097e28dfaf608c
IWANABETHATGUY <974153916@qq.com>
2020-04-04 20:02:56 +0800

    feat: fix request from maintainer

--- tests/lib/rules/no-computed-in-data.js
+++ tests/lib/rules/no-computed-in-data.js
@@ -68,6 +68,62 @@ ruleTester.run('no-computed-in-data', rule, {
       }
       </script>
       `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            data() {
+              return {
+                test: this.foo
+              }
+            },
+            computed: {
+              foo() {}
+            }
+          }
+        },
+        computed: {
+        }
+      }
+      </script>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            fn() { return this.foo }
+          }
+        },
+        computed: {
+          foo() {}
+        }
+      }
+      </script>
+      `
+    },
+    // should not warn when objectExpression is not a vue component
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      const a = {
+        data: {
+          a: this.test
+        },
+        computed: {
+          test() {},
+        }
+      }
+      </script>
+      `
     }
   ],
   invalid: [
@@ -94,6 +150,25 @@ ruleTester.run('no-computed-in-data', rule, {
       errors: [
         `Computed property 'this.test' can't use in data property.`,
         `Computed property 'this.foo' can't use in data property.`
+      ]
+    },
+    // should report when data is objectExpression, when this is a root component
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data: {
+          a: this.test
+        },
+        computed: {
+          test() {},
+        }
+      }
+      </script>
+      `,
+      errors: [
+        `Computed property 'this.test' can't use in data property.`
       ]
     },
     // same computed data referenced by multi data property
@@ -124,6 +199,71 @@ ruleTester.run('no-computed-in-data', rule, {
           message: `Computed property 'this.test' can't use in data property.`,
           line: 7,
           column: 16
+        }
+      ]
+    },
+    // should also report when computed data return an object
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            a: this.test.foo,
+            b: this.test.bar
+          }
+        },
+        computed: {
+          test() {
+            return {foo: 0, bar: 1}
+          },
+        }
+      }
+      </script>
+      `,
+      errors: [
+        {
+          message: `Computed property 'this.test' can't use in data property.`,
+          line: 6,
+          column: 16,
+          endColumn: 29,
+          endLine: 6
+        },
+        {
+          message: `Computed property 'this.test' can't use in data property.`,
+          line: 7,
+          column: 16,
+          endLine: 7,
+          endColumn: 29
+        }
+      ]
+    },
+    // should only report the data function scope computed property
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default {
+        data() {
+          return {
+            bar: this.foo,
+            fn() { return this.foo }
+          }
+        },
+        computed: {
+          foo() {}
+        }
+      }
+      </script>
+      `,
+      errors: [
+        {
+          message: `Computed property 'this.foo' can't use in data property.`,
+          line: 6,
+          column: 18,
+          endColumn: 26,
+          endLine: 6
         }
       ]
     }
