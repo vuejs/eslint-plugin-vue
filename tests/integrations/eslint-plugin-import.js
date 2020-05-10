c0e737c5e5960494b17653ecad7432e3cc5a8b40
Yosuke Ota <otameshiyo23@gmail.com>
2020-05-10 16:36:32 +0900

    Upgrade dependencies (#1121)
    * Upgrade dependencies
    
    * fix test error
    
    * fixed node v8 test

--- tests/integrations/eslint-plugin-import.js
+++ tests/integrations/eslint-plugin-import.js
@@ -11,6 +11,7 @@
 
 const cp = require('child_process')
 const path = require('path')
+const semver = require('semver')
 
 // -----------------------------------------------------------------------------
 // Tests
@@ -24,7 +25,7 @@ describe('Integration with eslint-plugin-import', () => {
   before(() => {
     originalCwd = process.cwd()
     process.chdir(path.join(__dirname, 'eslint-plugin-import'))
-    cp.execSync('npm i', { stdio: 'inherit', env: { npm_config_package_lock: 'false' }})
+    cp.execSync('npm i', { stdio: 'inherit' })
   })
   after(() => {
     process.chdir(originalCwd)
@@ -34,6 +35,10 @@ describe('Integration with eslint-plugin-import', () => {
   // eslint-plugin-vue had been breaking eslint-plugin-import if people use both at the same time.
   // This test is in order to prevent the regression.
   it('should lint without errors', () => {
+    if (!semver.satisfies(process.version, require(path.join(__dirname, 'eslint-plugin-import/node_modules/eslint/package.json')).engines.node)) {
+      return
+    }
+
     cp.execSync(`${ESLINT} a.vue`, { stdio: 'inherit' })
   })
 })
