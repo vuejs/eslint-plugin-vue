f537354c0b4741f9aa050ee4eb10e9105bd95e26
Yosuke Ota <otameshiyo23@gmail.com>
2020-05-24 06:27:55 +0900

    Add `vue/comma-style` rule (#1159)
    * Add `vue/comma-style` rule
    
    * update

---
+++ tests/lib/rules/comma-style.js
@@ -0,0 +1,124 @@
+/**
+ * @author Yosuke Ota
+ */
+'use strict'
+
+const { RuleTester } = require('eslint')
+const rule = require('../../../lib/rules/comma-style')
+
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: { ecmaVersion: 2018 }
+})
+
+tester.run('comma-style', rule, {
+  valid: [
+    `<template>
+      <CustomButton @click="() => fn({
+        a,
+        b
+      })" />
+    </template>`,
+    {
+      code: `
+        <template>
+          <CustomButton @click="($event,
+            data) => fn()" />
+        </template>`,
+      options: ['last', { exceptions: { ArrowFunctionExpression: false }}]
+    },
+    {
+      code: `
+        <template>
+          <CustomButton @click="($event
+            , data) => fn()" />
+        </template>`,
+      options: ['first', { exceptions: { ArrowFunctionExpression: false }}]
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        <template>
+          <CustomButton @click="() => fn({
+            a
+            , b
+          })" />
+        </template>`,
+      output: `
+        <template>
+          <CustomButton @click="() => fn({
+            a,
+             b
+          })" />
+        </template>`,
+      errors: [
+        {
+          message: "',' should be placed last.",
+          line: 5
+        }
+      ]
+    },
+    {
+      code: `
+        <template>
+          <CustomButton @click="($event
+            , data) => fn()" />
+        </template>`,
+      options: ['last', { exceptions: { ArrowFunctionExpression: false }}],
+      output: `
+        <template>
+          <CustomButton @click="($event,
+             data) => fn()" />
+        </template>`,
+      errors: [
+        {
+          message: "',' should be placed last.",
+          line: 4
+        }
+      ]
+    },
+    {
+      code: `
+        <template>
+          <CustomButton @click="($event,
+            data) => fn()" />
+        </template>`,
+      options: ['first', { exceptions: { ArrowFunctionExpression: false }}],
+      output: `
+        <template>
+          <CustomButton @click="($event
+            ,data) => fn()" />
+        </template>`,
+      errors: [
+        {
+          message: "',' should be placed first."
+          // line: 3 // eslint v7.0
+        }
+      ]
+    },
+    {
+      code: `
+        <template>
+          <CustomButton v-slot="foo,
+            bar" >
+            <div/>
+          </CustomButton>
+        </template>`,
+      options: ['first', { exceptions: { FunctionExpression: false }}],
+      output: `
+        <template>
+          <CustomButton v-slot="foo
+            ,bar" >
+            <div/>
+          </CustomButton>
+        </template>`,
+      errors: [
+        {
+          message: "',' should be placed first."
+          // line: 3 // eslint v7.0
+        }
+      ]
+    }
+  ]
+})
