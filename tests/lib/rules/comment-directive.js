b1a2b569f6e66544f0ed353a208dfda042108575
ota <y.ohta.z3@future.co.jp>
2020-05-28 14:44:47 +0900

    Add reportUnusedDisableDirectives option to `vue/comment-directive` rule

--- tests/lib/rules/comment-directive.js
+++ tests/lib/rules/comment-directive.js
@@ -130,6 +130,19 @@ describe('comment-directive', () => {
       assert.strictEqual(messages.length, 1)
       assert.strictEqual(messages[0].ruleId, 'no-unused-vars')
     })
+
+    it('disable specific rules if <!-- eslint-disable vue/no-duplicate-attributes ,, , vue/no-parsing-error -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable vue/no-duplicate-attributes ,, , vue/no-parsing-error -->
+          <div id id="a">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 0)
+    })
   })
 
   describe('eslint-disable-line', () => {
@@ -371,6 +384,171 @@ describe('comment-directive', () => {
       assert.deepEqual(messages.length, 2)
       assert.deepEqual(messages[0].ruleId, 'vue/no-parsing-error')
       assert.deepEqual(messages[1].ruleId, 'vue/no-duplicate-attributes')
+    })
+  })
+
+  describe('reportUnusedDisableDirectives', () => {
+    const linter = new eslint.CLIEngine({
+      parser: require.resolve('vue-eslint-parser'),
+      parserOptions: {
+        ecmaVersion: 2015
+      },
+      plugins: ['vue'],
+      rules: {
+        'no-unused-vars': 'error',
+        'vue/comment-directive': [
+          'error',
+          { reportUnusedDisableDirectives: true }
+        ],
+        'vue/no-parsing-error': 'error',
+        'vue/no-duplicate-attributes': 'error'
+      },
+      useEslintrc: false
+    })
+    it('report unused <!-- eslint-disable -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable -->
+          <div id="a">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 1)
+      assert.deepEqual(messages[0].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[0].message,
+        'Unused eslint-disable directive (no problems were reported).'
+      )
+      assert.deepEqual(messages[0].line, 3)
+      assert.deepEqual(messages[0].column, 11)
+    })
+
+    it('dont report unused <!-- eslint-disable -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable -->
+          <div id id="a">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 0)
+    })
+    it('disable and report unused <!-- eslint-disable -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable -->
+          <div id id="a">Hello</div>
+          <!-- eslint-enable -->
+          <!-- eslint-disable -->
+          <div id="b">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 1)
+      assert.deepEqual(messages[0].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[0].message,
+        'Unused eslint-disable directive (no problems were reported).'
+      )
+      assert.deepEqual(messages[0].line, 6)
+      assert.deepEqual(messages[0].column, 11)
+    })
+
+    it('report unused <!-- eslint-disable vue/no-duplicate-attributes, vue/no-parsing-error -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable vue/no-duplicate-attributes, vue/no-parsing-error -->
+          <div id="a">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 2)
+
+      assert.deepEqual(messages[0].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[0].message,
+        "Unused eslint-disable directive (no problems were reported from 'vue/no-duplicate-attributes')."
+      )
+      assert.deepEqual(messages[0].line, 3)
+      assert.deepEqual(messages[0].column, 31)
+
+      assert.deepEqual(messages[1].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[1].message,
+        "Unused eslint-disable directive (no problems were reported from 'vue/no-parsing-error')."
+      )
+      assert.deepEqual(messages[1].line, 3)
+      assert.deepEqual(messages[1].column, 60)
+    })
+
+    it('report unused <!-- eslint-disable-next-line vue/no-duplicate-attributes, vue/no-parsing-error -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable-next-line vue/no-duplicate-attributes, vue/no-parsing-error -->
+          <div id="a">Hello</div>
+          <div id id="b">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 4)
+
+      assert.deepEqual(messages[0].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[0].message,
+        "Unused eslint-disable-next-line directive (no problems were reported from 'vue/no-duplicate-attributes')."
+      )
+      assert.deepEqual(messages[0].line, 3)
+      assert.deepEqual(messages[0].column, 41)
+
+      assert.deepEqual(messages[1].ruleId, 'vue/comment-directive')
+      assert.deepEqual(
+        messages[1].message,
+        "Unused eslint-disable-next-line directive (no problems were reported from 'vue/no-parsing-error')."
+      )
+      assert.deepEqual(messages[1].line, 3)
+      assert.deepEqual(messages[1].column, 70)
+
+      assert.deepEqual(messages[2].ruleId, 'vue/no-parsing-error')
+      assert.deepEqual(messages[2].line, 5)
+      assert.deepEqual(messages[3].ruleId, 'vue/no-duplicate-attributes')
+      assert.deepEqual(messages[3].line, 5)
+    })
+
+    it('dont report used <!-- eslint-disable-next-line vue/no-duplicate-attributes, vue/no-parsing-error -->', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable-next-line vue/no-duplicate-attributes, vue/no-parsing-error -->
+          <div id id="a">Hello</div>
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 0)
+    })
+
+    it('dont report used, with duplicate eslint-disable', () => {
+      const code = `
+        <template>
+          <!-- eslint-disable -->
+          <!-- eslint-disable-next-line vue/no-duplicate-attributes, vue/no-parsing-error -->
+          <div id id="a">Hello</div><!-- eslint-disable-line vue/no-duplicate-attributes, vue/no-parsing-error -->
+        </template>
+      `
+      const messages = linter.executeOnText(code, 'test.vue').results[0]
+        .messages
+
+      assert.deepEqual(messages.length, 0)
     })
   })
 })
