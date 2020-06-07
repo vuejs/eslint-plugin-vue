4b4a1a1efea48b723a216294daf40737e84699a4
yosuke ota <otameshiyo23@gmail.com>
2020-06-07 16:12:09 +0900

    Fixed false positives for Vue 3 functional component in `vue/require-direct-export` rule.
    And, add option `disallowFunctionalComponentFunction` to revert to the old behavior.

--- tests/lib/rules/require-direct-export.js
+++ tests/lib/rules/require-direct-export.js
@@ -11,17 +11,17 @@
 const rule = require('../../../lib/rules/require-direct-export')
 const RuleTester = require('eslint').RuleTester
 
-const parserOptions = {
-  ecmaVersion: 2018,
-  sourceType: 'module',
-  ecmaFeatures: { jsx: true }
-}
-
 // ------------------------------------------------------------------------------
 // Tests
 // ------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  }
+})
 ruleTester.run('require-direct-export', rule, {
 
   valid: [
@@ -31,26 +31,184 @@ ruleTester.run('require-direct-export', rule, {
     },
     {
       filename: 'test.vue',
+      code: `export default {}`
+    },
+    {
+      filename: 'test.vue',
+      code: `export default {}`,
+      options: [{ disallowFunctionalComponentFunction: true }]
+    },
+    {
+      filename: 'test.js',
+      code: `export default Foo`
+    },
+    {
+      filename: 'test.vue',
       code: `
-            export default {}
-          `,
-      parserOptions
+      import { h } from 'vue'
+      export default function (props) {
+        return h('div', \`Hello! \${props.name}\`)
+      }
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default function Component () {
+        return h('div')
+      }
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default (props) => {
+        return h('div', \`Hello! \${props.name}\`)
+      }
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default props => h('div', props.msg)
+      `
     }
   ],
 
   invalid: [
-
     {
       filename: 'test.vue',
       code: `
-          const A = {};
-          export default A`,
-      parserOptions,
+      const A = {};
+      export default A`,
       errors: [{
         message: 'Expected the component literal to be directly exported.',
         type: 'ExportDefaultDeclaration',
         line: 3
       }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      function A(props) {
+        return h('div', props.msg)
+      };
+      export default A`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 5
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `export default function NoReturn() {}`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 1
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `export default function () {}`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 1
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `export default () => {}`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 1
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `export default () => {
+        const foo = () => {
+          return b
+        }
+      }`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 1
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `export default () => {
+        return
+      }`,
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 1
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      function A(props) {
+        return h('div', props.msg)
+      };
+      export default A`,
+      options: [{ disallowFunctionalComponentFunction: true }],
+      errors: [{
+        message: 'Expected the component literal to be directly exported.',
+        type: 'ExportDefaultDeclaration',
+        line: 5
+      }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default function (props) {
+        return h('div', \`Hello! \${props.name}\`)
+      }
+      `,
+      options: [{ disallowFunctionalComponentFunction: true }],
+      errors: ['Expected the component literal to be directly exported.']
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default function Component () {
+        return h('div')
+      }
+      `,
+      options: [{ disallowFunctionalComponentFunction: true }],
+      errors: ['Expected the component literal to be directly exported.']
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default (props) => {
+        return h('div', \`Hello! \${props.name}\`)
+      }
+      `,
+      options: [{ disallowFunctionalComponentFunction: true }],
+      errors: ['Expected the component literal to be directly exported.']
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      import { h } from 'vue'
+      export default props => h('div', props.msg)
+      `,
+      options: [{ disallowFunctionalComponentFunction: true }],
+      errors: ['Expected the component literal to be directly exported.']
     }
   ]
 })
