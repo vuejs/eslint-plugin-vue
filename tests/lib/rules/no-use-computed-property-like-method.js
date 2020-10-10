6b0b8609d92fd361240d1fb2e76f92b1beb59b17
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-10-10 10:17:18 +0900

    feat: getable methodProperties

--- tests/lib/rules/no-use-computed-property-like-method.js
+++ tests/lib/rules/no-use-computed-property-like-method.js
@@ -2,3 +2,134 @@
  * @author tyankatsu <https://github.com/tyankatsu0105>
  * See LICENSE file in root directory for full license.
  */
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/no-use-computed-property-like-method')
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: { ecmaVersion: 2015, sourceType: 'module' }
+})
+
+tester.run('no-use-computed-property-like-method', rule, {
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+        export default {
+          computed: {
+            name() {
+              return 'name';
+            }
+          },
+          methods: {
+            getName() {
+              return this.name
+            }
+          },
+        }
+      </script>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+        export default {
+          props: {
+            name: {
+              type: String
+            },
+          },
+          computed: {
+            isExpectedName() {
+              return this.name === 'name';
+            }
+          },
+          methods: {
+            getName() {
+              return this.isExpectedName
+            }
+          },
+        }
+      </script>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+        export default {
+          computed: {
+            name() {
+              return 'name';
+            },
+            isExpectedName() {
+              return this.name === 'name';
+            }
+          },
+          methods: {
+            getName() {
+              return this.isExpectedName
+            }
+          },
+        }
+      </script>
+      `
+    }
+  ],
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+        export default {
+          computed: {
+            name() {
+              return 'name';
+            }
+          },
+          methods: {
+            getName() {
+              return this.name()
+            }
+          }
+        }
+      </script>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+        export default {
+          props: {
+            name: {
+              type: String
+            },
+          },
+          computed: {
+            isExpectedName() {
+              return this.name === 'name';
+            }
+          },
+          methods: {
+            getName() {
+              return this.isExpectedName()
+            }
+          }
+        }
+      </script>
+      `
+    }
+  ]
+})
