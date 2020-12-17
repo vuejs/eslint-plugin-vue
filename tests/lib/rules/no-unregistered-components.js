3efbed05b75fde314f2c7a691e75cb72ba11f541
enzo360 <enzo.testa@360learning.com>
2020-12-17 09:37:23 +0100

    add tests

--- tests/lib/rules/no-unregistered-components.js
+++ tests/lib/rules/no-unregistered-components.js
@@ -458,7 +458,26 @@ tester.run('no-unregistered-components', rule, {
         }
         </script>
       `
-    }
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        <template>
+          <CustomComponentWithNamedSlots>
+            <template #slotA>
+              <div>Text</div>
+            </template>
+          </CustomComponentWithNamedSlots>
+        </template>
+        <script>
+        export default {
+          components: {
+            CustomComponentWithNamedSlots
+          }
+        }
+        </script>
+      `
+    },
   ],
   invalid: [
     {
@@ -668,6 +687,25 @@ tester.run('no-unregistered-components', rule, {
           line: 3
         }
       ]
-    }
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        <template>
+          <CustomComponentWithNamedSlots>
+            <template #slotA>
+              <div>Text</div>
+            </template>
+          </CustomComponentWithNamedSlots>
+        </template>
+      `,
+      errors: [
+        {
+          message:
+            'The "CustomComponentWithNamedSlots" component has been used but not registered.',
+          line: 3
+        }
+      ]
+    },
   ]
 })
