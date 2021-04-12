084bfe3a2eee243fbfed61baee2f1c5834d2a5ab
Alexander Sokolov <modex13@gmail.com>
2021-04-12 06:41:11 +0300

    New: Add `vue/no-invalid-model-keys` rule (#1466)
    * added no-invalid-model-keys rule
    
    * added no-invalid-model-keys to rules list
    
    * added no-invalid-model-keys tests
    
    * removed unused
    
    * added docs for no-invalid-model-keys
    
    * Update docs/rules/no-invalid-model-keys.md
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update docs/rules/no-invalid-model-keys.md
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update docs/rules/no-invalid-model-keys.md
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update docs/rules/no-invalid-model-keys.md
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update lib/rules/no-invalid-model-keys.js
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update lib/rules/no-invalid-model-keys.js
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update docs/rules/no-invalid-model-keys.md
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>
    
    * Update no-invalid-model-keys.md
    
    * Update no-invalid-model-keys.js
    
    * Update no-invalid-model-keys.js
    
    * Update no-invalid-model-keys.js
    
    * Update no-invalid-model-keys.js
    
    Co-authored-by: Alex Sokolov <4497128+Alex-Sokolov@users.noreply.github.com>
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>

---
+++ docs/rules/no-invalid-model-keys.md
@@ -0,0 +1,103 @@
+---
+pageClass: rule-details
+sidebarDepth: 0
+title: vue/no-invalid-model-keys
+description: require valid keys in model option
+---
+# vue/no-invalid-model-keys
+
+> require valid keys in model option
+
+
+## :book: Rule Details
+
+This rule is aimed at preventing invalid keys in model option.
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✓ GOOD */
+export default {
+  model: {
+    prop: 'list',
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✓ GOOD */
+export default {
+  model: {
+    event: 'update'
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✓ GOOD */
+export default {
+  model: {
+    prop: 'list',
+    event: 'update'
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✗ BAD */
+export default {
+  model: {
+    prop: 'list',
+    events: 'update'
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✗ BAD */
+export default {
+  model: {
+    props: 'list',
+    events: 'update'
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+<eslint-code-block :rules="{'vue/no-invalid-model-keys': ['error']}">
+```vue
+<script>
+/* ✗ BAD */
+export default {
+  model: {
+    name: 'checked',
+    props: 'list',
+    event: 'update'
+  }
+}
+</script>
+```
+</eslint-code-block>
+
+
+## :mag: Implementation
+
+- [Rule source](https://github.com/vuejs/eslint-plugin-vue/blob/master/lib/rules/no-invalid-model-keys.js)
+- [Test source](https://github.com/vuejs/eslint-plugin-vue/blob/master/tests/lib/rules/no-invalid-model-keys.js)
---
+++ tests/lib/rules/no-invalid-model-keys.js
@@ -0,0 +1,151 @@
+/**
+ * @fileoverview Prevents invalid keys in model option.
+ * @author Alex Sokolov
+ */
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-invalid-model-keys')
+const RuleTester = require('eslint').RuleTester
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  }
+})
+ruleTester.run('no-invalid-model-keys', rule, {
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            prop: 'list'
+          }
+        }
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            event: 'update'
+          }
+        }
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            prop: 'list',
+            event: 'update'
+          }
+        }
+      `
+    }
+  ],
+
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            props: 'list'
+          }
+        }
+      `,
+      errors: ["Invalid key 'props' in model option."]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            events: 'update'
+          }
+        }
+      `,
+      errors: ["Invalid key 'events' in model option."]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            props: 'list',
+            event: 'update'
+          }
+        }
+      `,
+      errors: ["Invalid key 'props' in model option."]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            prop: 'list',
+            events: 'update'
+          }
+        }
+      `,
+      errors: ["Invalid key 'events' in model option."]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            props: 'list',
+            events: 'update'
+          }
+        }
+      `,
+      errors: [
+        "Invalid key 'props' in model option.",
+        "Invalid key 'events' in model option."
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            prop: 'checked',
+            props: 'list',
+            event: 'update'
+          }
+        }
+      `,
+      errors: ["Invalid key 'props' in model option."]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        export default {
+          model: {
+            name: 'checked',
+            props: 'list',
+            event: 'update'
+          }
+        }
+      `,
+      errors: [
+        "Invalid key 'name' in model option.",
+        "Invalid key 'props' in model option."
+      ]
+    }
+  ]
+})
