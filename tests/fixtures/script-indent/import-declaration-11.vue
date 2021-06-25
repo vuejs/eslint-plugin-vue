06793cd442c42188723de9ad1719b2a33d5849ed
yosuke ota <otameshiyo23@gmail.com>
2021-06-25 18:29:38 +0900

    Fix false positives for trailing comma with import in `vue/script-indent` rule

---
+++ tests/fixtures/script-indent/import-declaration-11.vue
@@ -0,0 +1,7 @@
+<!--{ "options": [4, { "baseIndent": 1, "switchCase": 1 } ] }-->
+<script>
+    import {
+        computed, defineComponent, PropType, ref, watch,
+    } from '@vue/composition-api';
+</script>
+<!-- https://github.com/vuejs/eslint-plugin-vue/issues/1524 -->
