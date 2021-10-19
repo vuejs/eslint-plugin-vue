47270f3d66601b829fb789e4db4eec46d18ba2d8
Márton Csordás <csordasmarton92@gmail.com>
2021-10-19 17:06:42 +0200

    Fix review comments for no-deprecated-router-link-tag-prop
    - Use helper functions from utils to get attributes for nodes.
    - Add more test cases.
    - Handle both kebab-case and PascalCase version of components.

--- docs/rules/no-deprecated-router-link-tag-prop.md
+++ docs/rules/no-deprecated-router-link-tag-prop.md
@@ -20,16 +20,24 @@ This rule reports deprecated the `tag` attribute on `RouterLink` elements (remov
 <template>
   <!-- ✓ GOOD -->
   <RouterLink to="/">Home</RouterLink>
+  <router-link to="/">Home</router-link>
 
   <RouterLink to="/">
     <div>Home</div>
   </RouterLink>
 
+  <router-link to="/">
+    <div>Home</div>
+  </router-link>
+
   <NuxtLink tag="div" to="/">Home</NuxtLink>
+  <nuxt-link tag="div" to="/">Home</nuxt-link>
 
   <!-- ✗ BAD -->
   <RouterLink tag="div" to="/">Home</RouterLink>
+  <router-link tag="div" to="/">Home</router-link>
   <RouterLink :tag="someVariable" to="/">Home</RouterLink>
+  <router-link :tag="someVariable" to="/">Home</router-link>
 </template>
 ```
 
@@ -40,10 +48,15 @@ This rule reports deprecated the `tag` attribute on `RouterLink` elements (remov
 ```json
 {
   "vue/no-deprecated-router-link-tag-prop": ["error", {
-    "components": ['RouterLink', 'NuxtLink']
+    "components": ['RouterLink']
   }]
 }
 ```
+
+- `components` (`string[]`) ... Component names which will be checked with the `tag` attribute. default `['RouterLink']`.
+
+Note: this rule will check both `CamelCase` and `PascalCase` versions of the
+given component names.
 
 ### `{ "components": ['RouterLink', 'NuxtLink'] }`
 
@@ -52,11 +65,17 @@ This rule reports deprecated the `tag` attribute on `RouterLink` elements (remov
 ```vue
 <template>
   <!-- ✗ BAD -->
-  <NuxtLink tag="div" to="/">Home</NuxtLink>
-  <NuxtLink :tag="someVariable" to="/">Home</NuxtLink>
-
   <RouterLink tag="div" to="/">Home</RouterLink>
+  <router-link tag="div" to="/">Home</router-link>
+
   <RouterLink :tag="someVariable" to="/">Home</RouterLink>
+  <router-link :tag="someVariable" to="/">Home</router-link>
+
+  <NuxtLink tag="div" to="/">Home</NuxtLink>
+  <nuxt-link tag="div" to="/">Home</nuxt-link>
+
+  <NuxtLink :tag="someVariable" to="/">Home</NuxtLink>
+  <nuxt-link :tag="someVariable" to="/">Home</nuxt-link>
 </template>
 ```
 
--- tests/lib/rules/no-deprecated-router-link-tag-prop.js
+++ tests/lib/rules/no-deprecated-router-link-tag-prop.js
@@ -29,9 +29,27 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
       filename: 'test.vue',
       code: `
       <template>
+        <router-link to="/">Home</router-link>
+      </template>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
         <RouterLink to="/">
           <div>Home</div>
         </RouterLink>
+      </template>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link to="/">
+          <div>Home</div>
+        </router-link>
       </template>
       `
     },
@@ -47,9 +65,27 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
       filename: 'test.vue',
       code: `
       <template>
+        <nuxt-link to="/">Home</nuxt-link>
+      </template>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
         <NuxtLink to="/">
           <div>Home</div>
         </NuxtLink>
+      </template>
+      `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <nuxt-link to="/">
+          <div>Home</div>
+        </nuxt-link>
       </template>
       `
     }
@@ -75,6 +111,22 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
       filename: 'test.vue',
       code: `
       <template>
+        <router-link tag="div" to="/">Home</router-link>
+      </template>
+      `,
+      errors: [
+        {
+          message:
+            "'tag' property on 'router-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 22
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
         <RouterLink tag="div" to="/">Home</RouterLink>
       </template>
       `,
@@ -92,6 +144,40 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
       filename: 'test.vue',
       code: `
       <template>
+        <RouterLink tag="div" to="/">Home</RouterLink>
+      </template>
+      `,
+      options: [{ components: ['router-link'] }],
+      errors: [
+        {
+          message:
+            "'tag' property on 'RouterLink' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 21
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link tag="div" to="/">Home</router-link>
+      </template>
+      `,
+      options: [{ components: ['RouterLink'] }],
+      errors: [
+        {
+          message:
+            "'tag' property on 'router-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 22
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
         <RouterLink :tag="someVariable" to="/">Home</RouterLink>
       </template>
       `,
@@ -101,6 +187,22 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
             "'tag' property on 'RouterLink' component is deprecated. Use scoped slots instead.",
           line: 3,
           column: 22
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link :tag="someVariable" to="/">Home</router-link>
+      </template>
+      `,
+      errors: [
+        {
+          message:
+            "'tag' property on 'router-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 23
         }
       ]
     },
@@ -118,6 +220,23 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
             "'tag' property on 'RouterLink' component is deprecated. Use scoped slots instead.",
           line: 3,
           column: 22
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link :tag="someVariable" to="/">Home</router-link>
+      </template>
+      `,
+      options: [{ components: ['RouterLink'] }],
+      errors: [
+        {
+          message:
+            "'tag' property on 'router-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 23
         }
       ]
     },
@@ -142,6 +261,23 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
       filename: 'test.vue',
       code: `
       <template>
+        <nuxt-link tag="div" to="/">Home</nuxt-link>
+      </template>
+      `,
+      options: [{ components: ['NuxtLink'] }],
+      errors: [
+        {
+          message:
+            "'tag' property on 'nuxt-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 20
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
         <NuxtLink :tag="someVariable" to="/">Home</NuxtLink>
       </template>
       `,
@@ -152,6 +288,23 @@ tester.run('no-deprecated-router-link-tag-prop', rule, {
             "'tag' property on 'NuxtLink' component is deprecated. Use scoped slots instead.",
           line: 3,
           column: 20
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <nuxt-link :tag="someVariable" to="/">Home</nuxt-link>
+      </template>
+      `,
+      options: [{ components: ['NuxtLink'] }],
+      errors: [
+        {
+          message:
+            "'tag' property on 'nuxt-link' component is deprecated. Use scoped slots instead.",
+          line: 3,
+          column: 21
         }
       ]
     }
