dc0e3ea9cdc8a8930cb24d472d3b04ee47d42626
Yosuke Ota <otameshiyo23@gmail.com>
2021-10-30 13:09:17 +0900

    Upgrade vue-eslint-parser to use patch. (#1691)
    * Upgrade vue-eslint-parser to use patch.
    
    https://github.com/vuejs/vue-eslint-parser/releases/tag/v8.0.1
    
    * fix

--- tests/lib/rules/no-async-in-computed-properties.js
+++ tests/lib/rules/no-async-in-computed-properties.js
@@ -307,6 +307,31 @@ ruleTester.run('no-async-in-computed-properties', rule, {
         }
         `,
       parserOptions
+    },
+    {
+      // https://github.com/vuejs/eslint-plugin-vue/issues/1690
+      filename: 'test.vue',
+      parser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020
+      },
+      code: `
+      <template>
+        <div class="f-c" style="height: 100%;">
+        </div>
+      </template>
+      <script setup>
+      import { ref, computed } from 'vue' // each time uncomment error will print. anything from 'vue'
+      import { useStore } from 'vuex' // others like this is ok
+      </script>
+      <script>
+      export default {
+        name: 'App',
+        components: {
+        },
+      }
+      </script>`
     }
   ],
 
