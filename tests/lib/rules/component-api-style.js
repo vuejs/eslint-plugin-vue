7796cb5dda0b99a5a558ab637b03738f702bef8c
Flo Edelmann <florian-edelmann@online.de>
2021-11-11 00:49:33 +0100

    Add `composition-vue2` api style

--- tests/lib/rules/component-api-style.js
+++ tests/lib/rules/component-api-style.js
@@ -159,6 +159,28 @@ tester.run('component-api-style', rule, {
       }
       </script>
       `
+    },
+    {
+      filename: 'test.vue',
+      options: [['composition-vue2']],
+      code: `
+      <script>
+      import { ref } from 'vue'
+      export default {
+        setup() {
+          const msg = ref('Hello World!')
+          // ...
+          return {
+            msg,
+            // ...
+          }
+        },
+        render(h) {
+          return h('foo', this.msg)
+        }
+      }
+      </script>
+      `
     }
   ],
   invalid: [
@@ -557,6 +579,231 @@ tester.run('component-api-style', rule, {
         {
           message:
             'Options API is not allowed in your project. `expose` option is the API of Options API. Use Composition API instead.',
+          line: 31,
+          column: 9
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script setup>
+      </script>
+      `,
+      options: [['composition-vue2']],
+      errors: [
+        {
+          message:
+            '`<script setup>` is not allowed in your project. Use Composition API (Vue 2) instead.',
+          line: 2,
+          column: 7
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      options: [['composition-vue2']],
+      code: `
+      <script>
+      export default {
+        data () {
+          return {
+            msg: 'Hello World!',
+            // ...
+          }
+        },
+        // ...
+      }
+      </script>
+      `,
+      errors: [
+        {
+          message:
+            'Options API is not allowed in your project. `data` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 4,
+          column: 9
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      options: [['composition-vue2']],
+      code: `
+      <script>
+      export default {
+        mixins: [],
+        extends: {},
+        // state
+        data () { return {} },
+        computed: {},
+        methods: {},
+        watch: {},
+        provide: {},
+        inject: {},
+        // lifecycle
+        beforeCreate() {},
+        created() {},
+        beforeMount() {},
+        mounted() {},
+        beforeUpdate() {},
+        updated() {},
+        activated() {},
+        deactivated() {},
+        beforeDestroy() {},
+        beforeUnmount() {},
+        destroyed() {},
+        unmounted() {},
+        render() {}, // allowed
+        renderTracked() {},
+        renderTriggered() {},
+        errorCaptured() {},
+        // public API
+        expose: [],
+      }
+      </script>
+      `,
+      errors: [
+        {
+          message:
+            'Options API is not allowed in your project. `mixins` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 4,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `extends` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 5,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `data` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 7,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `computed` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 8,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `methods` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 9,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `watch` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 10,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `provide` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 11,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `inject` option is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 12,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `beforeCreate` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 14,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `created` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 15,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `beforeMount` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 16,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `mounted` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 17,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `beforeUpdate` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 18,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `updated` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 19,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `activated` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 20,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `deactivated` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 21,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `beforeDestroy` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 22,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `beforeUnmount` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 23,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `destroyed` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 24,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `unmounted` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 25,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `renderTracked` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 27,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `renderTriggered` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 28,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `errorCaptured` lifecycle hook is the API of Options API. Use Composition API (Vue 2) instead.',
+          line: 29,
+          column: 9
+        },
+        {
+          message:
+            'Options API is not allowed in your project. `expose` option is the API of Options API. Use Composition API (Vue 2) instead.',
           line: 31,
           column: 9
         }
