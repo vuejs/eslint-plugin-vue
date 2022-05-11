19c6e8656ea009f40403f2a8a5b4caf4f7cbd33a
Flo Edelmann <florian-edelmann@online.de>
2022-05-11 08:31:32 +0200

    Enable `vue/html-closing-bracket-*` for top-level tags (#1883)
    * Enable `vue/html-closing-bracket-*` for top-level tags
    
    * Switch to much simpler `defineDocumentVisitor`
    
    * Add type annotation again

--- tests/lib/rules/html-closing-bracket-newline.js
+++ tests/lib/rules/html-closing-bracket-newline.js
@@ -337,6 +337,156 @@ tester.run('html-closing-bracket-newline', rule, {
         'Expected 1 line break before closing bracket, but no line breaks found.',
         'Expected 1 line break before closing bracket, but no line breaks found.'
       ]
+    },
+    {
+      code: `
+        <template
+        >
+        </template
+        >
+        <script
+        >
+        </script
+        >
+        <style
+        ></style
+        >
+      `,
+      output: `
+        <template>
+        </template>
+        <script>
+        </script>
+        <style></style>
+      `,
+      options: [
+        {
+          singleline: 'never',
+          multiline: 'never'
+        }
+      ],
+      errors: [
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 2,
+          column: 18,
+          endLine: 3,
+          endColumn: 9
+        },
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 4,
+          column: 19,
+          endLine: 5,
+          endColumn: 9
+        },
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 6,
+          column: 16,
+          endLine: 7,
+          endColumn: 9
+        },
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 8,
+          column: 17,
+          endLine: 9,
+          endColumn: 9
+        },
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 10,
+          column: 15,
+          endLine: 11,
+          endColumn: 9
+        },
+        {
+          message:
+            'Expected no line breaks before closing bracket, but 1 line break found.',
+          line: 11,
+          column: 17,
+          endLine: 12,
+          endColumn: 9
+        }
+      ]
+    },
+    {
+      code: `
+        <template>
+        </template>
+        <script>
+        </script>
+        <style></style>
+      `,
+      output: `
+        <template
+>
+        </template
+>
+        <script
+>
+        </script
+>
+        <style
+></style
+>
+      `,
+      options: [
+        {
+          singleline: 'always',
+          multiline: 'always'
+        }
+      ],
+      errors: [
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 2,
+          column: 18,
+          endColumn: 18
+        },
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 3,
+          column: 19,
+          endColumn: 19
+        },
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 4,
+          column: 16,
+          endColumn: 16
+        },
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 5,
+          column: 17,
+          endColumn: 17
+        },
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 6,
+          column: 15,
+          endColumn: 15
+        },
+        {
+          message:
+            'Expected 1 line break before closing bracket, but no line breaks found.',
+          line: 6,
+          column: 23,
+          endColumn: 23
+        }
+      ]
     }
   ]
 })
