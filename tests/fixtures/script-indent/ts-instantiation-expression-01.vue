99e9adc5b9ce284388cba530ee8613f636d3cb0b
yosuke ota <otameshiyo23@gmail.com>
2022-11-22 01:17:58 +0900

    fix

--- tests/fixtures/script-indent/ts-instantiation-expression-01.vue
+++ tests/fixtures/script-indent/ts-instantiation-expression-01.vue
@@ -1,4 +1,4 @@
-<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.26.0"}-->
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.26.0"}}-->
 <script lang="ts">
 const ErrorMap = Map
   <
