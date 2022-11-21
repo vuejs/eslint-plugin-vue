a3fb7b9c476cbd59a2a8144899c0ac326d332870
yosuke ota <otameshiyo23@gmail.com>
2022-11-22 07:54:00 +0900

    support for satisfies op

---
+++ tests/fixtures/script-indent/ts-satisfies-operators-01.vue
@@ -0,0 +1,8 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.44.0" } }-->
+<script lang="ts">
+var a =
+  {} satisfies
+    Foo
+var b =
+  {} satisfies Bar
+</script>
