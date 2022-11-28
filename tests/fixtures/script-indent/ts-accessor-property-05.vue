1af9f580b056be38c15c6fbd6c90b4e1100c40f2
yosuke ota <otameshiyo23@gmail.com>
2022-11-28 16:44:15 +0900

    add test cases

---
+++ tests/fixtures/script-indent/ts-accessor-property-05.vue
@@ -0,0 +1,12 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.44.1-alpha.15"}}-->
+<script lang="ts">
+class Foo {
+  accessor
+  [
+    1
+  ]
+    =
+    2
+  ;
+}
+</script>
