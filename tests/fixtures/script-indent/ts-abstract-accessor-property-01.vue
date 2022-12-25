e42f7371a4e396eab1a4b405af2a61cc1d165651
Yosuke Ota <otameshiyo23@gmail.com>
2022-12-25 18:15:36 +0900

    Improve indent rule to support more ts syntax (#2042)
    Co-authored-by: Flo Edelmann <florian-edelmann@online.de>

---
+++ tests/fixtures/script-indent/ts-abstract-accessor-property-01.vue
@@ -0,0 +1,12 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.44.1-alpha.15"}}-->
+<script lang="ts">
+abstract class Foo {
+  abstract accessor
+  foo:
+    number
+  // parser v5 does not parse value.
+  //    =
+  //    1
+  ;
+}
+</script>
---
+++ tests/fixtures/script-indent/ts-accessor-property-01.vue
@@ -0,0 +1,8 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}, "requirements": { "@typescript-eslint/parser": ">=5.44.1-alpha.15"}}-->
+<script lang="ts">
+class Foo {
+  accessor
+  foo
+  ;
+}
+</script>
