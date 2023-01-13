20e842460316bfa87f098c72c035bc2f98bc89a4
ota-meshi <otameshiyo23@gmail.com>
2023-01-13 18:02:33 +0900

    fix

--- docs/.vitepress/vite-plugin.ts
+++ docs/.vitepress/vite-plugin.ts
@@ -1,7 +1,7 @@
 import type { UserConfig } from 'vitepress'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { viteCommonjs as baseViteCommonjs } from '@originjs/vite-plugin-commonjs'
+import esbuild from 'esbuild'
 type Plugin = Extract<
   NonNullable<NonNullable<UserConfig['vite']>['plugins']>[number],
   { name: string }
@@ -27,27 +27,72 @@ export function vitePluginRequireResolve(): Plugin {
 }
 
 export function viteCommonjs(): Plugin {
-  const base = baseViteCommonjs({
-    include: [libRoot],
-    skipPreBuild: true
-  }) as Plugin
   return {
-    ...base,
-    // The `@originjs/vite-plugin-commonjs` is 'serve' only, but use it in 'build' as well.
+    name: 'vite-plugin-cjs-to-esm',
     apply: () => true,
-    async transform(code, id, options) {
-      if (typeof base.transform !== 'function') {
-        return null
+    async transform(code, id) {
+      if (!id.startsWith(libRoot)) {
+        return undefined
       }
-      const result = await base.transform.call(this, code, id, options)
-      if (result && typeof result === 'object' && result.code) {
-        return {
-          ...result,
-          // Replace it with null, because blanks can be given to the sourcemap and cause an error.
-          map: undefined
-        }
+      const base = transformRequire(code)
+      try {
+        const transformed = esbuild.transformSync(base, {
+          format: 'esm'
+        })
+        return transformed.code
+      } catch (e) {
+        console.error('Transform error. base code:\n' + base, e)
       }
-      return result
+      return undefined
     }
   }
+}
+
+/**
+ * Transform `require()` to `import`
+ * Inspired by `@originjs/vite-plugin-commonjs`.
+ * https://github.com/originjs/vite-plugins/tree/main/packages/vite-plugin-commonjs
+ */
+function transformRequire(code: string) {
+  if (!code.includes('require')) {
+    return code
+  }
+  const modules = new Map()
+  const replaced = code.replace(
+    /(\/\/[^\n\r]*|\/\*[\s\S]*?\*\/)|\brequire\s*\(\s*(["'].*?["'])\s*\)/gu,
+    (match, comment, moduleString) => {
+      if (comment) {
+        return match
+      }
+
+      let id =
+        '__' + moduleString.replace(/[^a-zA-Z0-9_$]+/gu, '_') + randomString()
+      while (code.includes(id) || modules.has(id)) {
+        id += randomString()
+      }
+      modules.set(id, moduleString)
+      return id
+    }
+  )
+
+  return (
+    [...modules]
+      .map(([id, moduleString]) => {
+        return `import * as __temp_${id} from ${moduleString};
+const ${id} = __temp_${id}.default || __temp_${id};
+`
+      })
+      .join('') +
+    ';\n' +
+    replaced
+  )
+}
+
+function randomString() {
+  const code = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  let result = ''
+  for (let index = 0; index < 6; index++) {
+    result += code[Math.floor(Math.random() * code.length)]
+  }
+  return result
 }
