99aaa101a8dff51122ad59dbd85ca3886621d502
ota-meshi <otameshiyo23@gmail.com>
2023-03-22 16:25:41 +0900

    Use `@eslint-community` packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -131,7 +131,9 @@ module.exports = {
     'unicorn/no-useless-undefined': 'off',
     'unicorn/prefer-optional-catch-binding': 'off', // not supported by current ESLint parser version
     'unicorn/prefer-module': 'off',
-    'unicorn/prevent-abbreviations': 'off'
+    'unicorn/prevent-abbreviations': 'off',
+
+    'require-eslint-community': ['error']
   },
   overrides: [
     {
