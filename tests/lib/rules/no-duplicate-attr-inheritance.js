43d287c52f63de26e473583c5a547139c9b49255
yosuke ota <otameshiyo23@gmail.com>
2023-05-17 07:42:02 +0900

    add tests

--- tests/lib/rules/no-duplicate-attr-inheritance.js
+++ tests/lib/rules/no-duplicate-attr-inheritance.js
@@ -119,6 +119,38 @@ ruleTester.run('no-duplicate-attr-inheritance', rule, {
         </script>
       `,
       errors: ['Set "inheritAttrs" to false.']
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script>
+      export default { inheritAttrs: true }
+      </script>
+      <script setup>
+      </script>
+      <template><div v-bind="$attrs" /></template>
+      `,
+      errors: [
+        {
+          message: 'Set "inheritAttrs" to false.',
+          line: 7
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <script setup>
+      defineOptions({ inheritAttrs: true })
+      </script>
+      <template><div v-bind="$attrs" /></template>
+      `,
+      errors: [
+        {
+          message: 'Set "inheritAttrs" to false.',
+          line: 5
+        }
+      ]
     }
   ]
 })
