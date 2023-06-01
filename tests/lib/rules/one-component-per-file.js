7baf1ab2ec0f2a70d05ab547789b9bf7ba1d58f4
candy-Tong <563378816@qq.com>
2023-06-01 17:00:31 +0800

    fix: [one-component-per-file] do not check createApp function written by the developer #2201

--- tests/lib/rules/one-component-per-file.js
+++ tests/lib/rules/one-component-per-file.js
@@ -54,6 +54,21 @@ ruleTester.run('one-component-per-file', rule, {
         Vue.mixin({})
         Vue.component('name', {})
       `
+    },
+    {
+      filename: 'test.js',
+      code: `
+        import { createApp } from 'vue'
+        createApp({})
+      `
+    },
+    {
+      filename: 'test.js',
+      code: `
+        import { createApp } from 'other.js'
+        createApp({})
+        createApp({})
+      `
     }
   ],
   invalid: [
@@ -90,6 +105,18 @@ ruleTester.run('one-component-per-file', rule, {
       code: `
         Vue.component('name', {})
         export default {}
+      `,
+      errors: [
+        'There is more than one component in this file.',
+        'There is more than one component in this file.'
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        import { createApp } from 'vue'
+        createApp({})
+        createApp({})
       `,
       errors: [
         'There is more than one component in this file.',
