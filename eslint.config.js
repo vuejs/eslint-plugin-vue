3b9fcd2eae88b5c0ac4d81cbcfce673274993e51
Flo Edelmann <git@flo-edelmann.de>
2023-07-18 16:18:20 +0200

    Enable `eslint-plugin/prefer-replace-text` rule

--- eslint.config.js
+++ eslint.config.js
@@ -145,6 +145,7 @@ module.exports = [
       'eslint-plugin/report-message-format': ['error', "^[A-Z`'{].*\\.$"],
       'eslint-plugin/meta-property-ordering': 'error',
       'eslint-plugin/prefer-placeholders': 'error',
+      'eslint-plugin/prefer-replace-text': 'error',
       'eslint-plugin/test-case-property-ordering': 'error',
       'eslint-plugin/test-case-shorthand-strings': 'error',
 
