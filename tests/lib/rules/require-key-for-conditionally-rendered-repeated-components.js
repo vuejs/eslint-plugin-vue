11769be604f0a238db67e767687371581a4dd639
felipe <felipe.melendez@pilot.com>
2023-08-28 07:53:54 -0700

    create tests

---
+++ tests/lib/rules/require-key-for-conditionally-rendered-repeated-components.js
@@ -0,0 +1,261 @@
+/**
+ * @author Felipe Melendez
+ * See LICENSE file in root directory for full license.
+ */
+'use strict'
+
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/require-key-for-conditionally-rendered-repeated-components')
+
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  }
+})
+
+tester.run('require-key-for-conditionally-rendered-repeated-components', rule, {
+  valid: [
+    {
+      filename: 'test.vue',
+      code: `
+        <template>
+    <div>
+              <CustomComponent v-if='some-condition' key='key1' />
+              <CustomComponent v-else key='key2' />
+    </div>
+        </template>
+        <script>
+        export default {
+            components: {
+                CustomComponent
+            }
+        }
+        </script>
+        `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        <template>
+    <div>
+              <CustomComponent v-if='some-condition' />
+              <CustomComponent />
+    </div>
+        </template>
+        <script>
+        export default {
+            components: {
+                CustomComponent
+            }
+        }
+        </script>
+        `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+        <template>
+    <div>
+                <div v-if='some-condition' >
+                  <CustomComponent />
+                </div>
+              <CustomComponent v-else />
+    </div>
+        </template>
+        <script>
+        export default {
+            components: {
+                CustomComponent
+            }
+        }
+        </script>
+        `
+    }
+  ],
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: `
+          <template>
+              <div>
+                  <OuterComponent v-if='showOuterComponent'>
+                      <InnerComponent v-if='showInnerComponent1' />
+                      <InnerComponent v-else />
+                  </OuterComponent>
+                  <OuterComponent v-else>
+                      <InnerComponent :key='key1' />
+                      <InnerComponent :key='key2' />
+                  </OuterComponent>
+              </div>
+          </template>
+          <script>
+          export default {
+              components: {
+                  OuterComponent,
+                  InnerComponent
+              }
+          }
+          </script>
+          `,
+      output: `
+          <template>
+              <div>
+                  <OuterComponent key='outer-component-1' v-if='showOuterComponent'>
+                      <InnerComponent key='inner-component-1' v-if='showInnerComponent1' />
+                      <InnerComponent key='inner-component-2' v-else />
+                  </OuterComponent>
+                  <OuterComponent key='outer-component-2' v-else>
+                      <InnerComponent :key='key1' />
+                      <InnerComponent :key='key2' />
+                  </OuterComponent>
+              </div>
+          </template>
+          <script>
+          export default {
+              components: {
+                  OuterComponent,
+                  InnerComponent
+              }
+          }
+          </script>
+          `,
+      errors: [
+        {
+          message:
+            "Conditionally rendered repeated component 'OuterComponent' expected to have a 'key' attribute.",
+          line: 4
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'InnerComponent' expected to have a 'key' attribute.",
+          line: 5
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'InnerComponent' expected to have a 'key' attribute.",
+          line: 6
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'OuterComponent' expected to have a 'key' attribute.",
+          line: 8
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+              <template>
+                  <div>
+                      <OuterComponent v-if='showOuterComponent'>
+                          <InnerComponent v-if='showInnerComponent1'>
+                              <DifferentScopeComponent />
+                          </InnerComponent>
+                          <InnerComponent v-else />
+                      </OuterComponent>
+                      <OuterComponent v-else>
+                          <InnerComponent />
+                          <InnerComponent />
+                      </OuterComponent>
+                      <DifferentScopeComponent />
+                  </div>
+              </template>
+              <script>
+              export default {
+                  components: {
+                      OuterComponent,
+                      InnerComponent
+                  }
+              }
+              </script>
+              `,
+      output: `
+              <template>
+                  <div>
+                      <OuterComponent key='outer-component-1' v-if='showOuterComponent'>
+                          <InnerComponent key='inner-component-1' v-if='showInnerComponent1'>
+                              <DifferentScopeComponent />
+                          </InnerComponent>
+                          <InnerComponent key='inner-component-2' v-else />
+                      </OuterComponent>
+                      <OuterComponent key='outer-component-2' v-else>
+                          <InnerComponent />
+                          <InnerComponent />
+                      </OuterComponent>
+                      <DifferentScopeComponent />
+                  </div>
+              </template>
+              <script>
+              export default {
+                  components: {
+                      OuterComponent,
+                      InnerComponent
+                  }
+              }
+              </script>
+              `,
+      errors: [
+        {
+          message:
+            "Conditionally rendered repeated component 'OuterComponent' expected to have a 'key' attribute.",
+          line: 4
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'InnerComponent' expected to have a 'key' attribute.",
+          line: 5
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'InnerComponent' expected to have a 'key' attribute.",
+          line: 8
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'OuterComponent' expected to have a 'key' attribute.",
+          line: 10
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+              <template>
+            <div>
+                      <div v-if='someCondition' />
+                      <div v-else-if='otherCondition' />
+                      <div v-else />
+            </div>
+              </template>
+              `,
+      output: `
+              <template>
+            <div>
+                      <div key='div-1' v-if='someCondition' />
+                      <div key='div-2' v-else-if='otherCondition' />
+                      <div key='div-3' v-else />
+            </div>
+              </template>
+              `,
+      errors: [
+        {
+          message:
+            "Conditionally rendered repeated component 'div' expected to have a 'key' attribute.",
+          line: 4
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'div' expected to have a 'key' attribute.",
+          line: 5
+        },
+        {
+          message:
+            "Conditionally rendered repeated component 'div' expected to have a 'key' attribute.",
+          line: 6
+        }
+      ]
+    }
+  ]
+})
