53d3ba076432e302abd62776c40979d55bb60106
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-24 11:45:54 +0800

    chore: test

--- tests/lib/rules/component-name-in-template-casing.js
+++ tests/lib/rules/component-name-in-template-casing.js
@@ -867,7 +867,7 @@ tester.run('component-name-in-template-casing', rule, {
       `,
       output: `
         <template>
-          <Component />
+          <component />
           <Suspense />
           <Teleport />
           <ClientOnly />
@@ -876,7 +876,6 @@ tester.run('component-name-in-template-casing', rule, {
       `,
       options: ['PascalCase', { registeredComponentsOnly: false }],
       errors: [
-        'Component name "component" is not PascalCase.',
         'Component name "suspense" is not PascalCase.',
         'Component name "teleport" is not PascalCase.',
         'Component name "client-only" is not PascalCase.',
@@ -1033,7 +1032,7 @@ tester.run('component-name-in-template-casing', rule, {
                 <HelloWorld3 />
                 <HelloWorld4 />
                 <HelloWorld5 />
-                <Component />
+                <component />
               </template>
             `,
             errors: [
@@ -1065,11 +1064,6 @@ tester.run('component-name-in-template-casing', rule, {
               {
                 message: 'Component name "hello-world5" is not PascalCase.',
                 line: 18,
-                column: 17
-              },
-              {
-                message: 'Component name "component" is not PascalCase.',
-                line: 19,
                 column: 17
               }
             ]
