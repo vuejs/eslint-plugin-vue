fe24ff35600c54d994000d66382175ccadfebbb8
KazumaOhashi <kazuma0129.work@gmail.com>
2023-11-14 17:46:38 +0900

    Add new option to specify tags to ignore `vue/no-deprecated-slot-attribute`

--- tests/lib/rules/no-deprecated-slot-attribute.js
+++ tests/lib/rules/no-deprecated-slot-attribute.js
@@ -46,7 +46,17 @@ tester.run('no-deprecated-slot-attribute', rule, {
       <LinkList>
         <a />
       </LinkList>
-    </template>`
+    </template>`,
+    {
+      code: `<template>
+      <LinkList>
+        <one slot="one" />
+        <two slot="two" />
+        <my-component slot="my-component-slot" />
+      </LinkList>
+    </template>`,
+      options: [{ ignore: ['one', 'two', 'my-component'] }]
+    }
   ],
   invalid: [
     {
@@ -594,6 +604,26 @@ tester.run('no-deprecated-slot-attribute', rule, {
         '`slot` attributes are deprecated.',
         '`slot` attributes are deprecated.'
       ]
+    },
+    {
+      code: `
+      <template>
+        <my-component>
+          <one slot="one">
+            A
+          </one>
+          <two slot="two">
+            B
+          </two>
+        </my-component>
+      </template>`,
+      output: null,
+      options: [
+        {
+          ignore: ['one']
+        }
+      ],
+      errors: ['`slot` attributes are deprecated.']
     }
   ]
 })
