06ba377540cce324e6535560ef7da6458c38e5f6
Mussin Benarbia <mussin.benarbia@gmail.com>
2023-11-24 17:42:51 +0900

    refactor: allow multiple attributes

--- tests/lib/rules/enforce-style-attribute.js
+++ tests/lib/rules/enforce-style-attribute.js
@@ -17,45 +17,62 @@ const tester = new RuleTester({
 
 tester.run('enforce-style-attribute', rule, {
   valid: [
-    // With default options
+    // Default (scoped)
     {
       filename: 'test.vue',
       code: '<template></template><script></script><style scoped></style>'
-    },
-    {
-      filename: 'test.vue',
-      code: '<template></template><script></script><style module></style>'
-    },
-    {
-      filename: 'test.vue',
-      code: '<template></template><script></script><style lang=scss" "src="../path/to/style.scss" scoped></style>'
     },
     // With scoped option
     {
       filename: 'test.vue',
       code: '<template></template><script></script><style scoped></style>',
-      options: ['scoped']
-    },
-    {
-      filename: 'test.vue',
-      code: '<template></template><script></script><style lang=scss" "src="../path/to/style.scss" scoped></style>',
-      options: ['scoped']
+      options: [{ allows: ['scoped'] }]
     },
     // With module option
     {
       filename: 'test.vue',
       code: '<template></template><script></script><style module></style>',
-      options: ['module']
+      options: [{ allows: ['module'] }]
+    },
+    // With plain option
+    {
+      filename: 'test.vue',
+      code: '<template></template><script></script><style></style>',
+      options: [{ allows: ['no-attributes'] }]
+    },
+    // With all options
+    {
+      filename: 'test.vue',
+      code: '<template></template><script></script><style scoped></style>',
+      options: [{ allows: ['scoped', 'module', 'no-attributes'] }]
+    },
+    {
+      filename: 'test.vue',
+      code: '<template></template><script></script><style module></style>',
+      options: [{ allows: ['scoped', 'module', 'no-attributes'] }]
+    },
+    {
+      filename: 'test.vue',
+      code: '<template></template><script></script><style></style>',
+      options: [{ allows: ['scoped', 'module', 'no-attributes'] }]
     }
   ],
   invalid: [
-    // With default options
+    // With default (scoped)
     {
       code: `<template></template><script></script><style></style>`,
       errors: [
         {
           message:
-            'The <style> tag should have either the scoped or module attribute.'
+            '<style> tags without attributes are not allowed. Allowed: scoped'
+        }
+      ]
+    },
+    {
+      code: `<template></template><script></script><style module></style>`,
+      errors: [
+        {
+          message: 'The module attribute is not allowed. Allowed: scoped'
         }
       ]
     },
@@ -64,38 +81,71 @@ tester.run('enforce-style-attribute', rule, {
       code: `<template></template><script></script><style></style>`,
       errors: [
         {
-          message: 'The <style> tag should have the scoped attribute.'
+          message:
+            '<style> tags without attributes are not allowed. Allowed: scoped'
         }
       ],
-      options: ['scoped']
+      options: [{ allows: ['scoped'] }]
     },
     {
       code: `<template></template><script></script><style module></style>`,
       errors: [
         {
-          message: 'The <style> tag should have the scoped attribute.'
+          message: 'The module attribute is not allowed. Allowed: scoped'
         }
       ],
-      options: ['scoped']
+      options: [{ allows: ['scoped'] }]
     },
     // With module option
     {
       code: `<template></template><script></script><style></style>`,
       errors: [
         {
-          message: 'The <style> tag should have the module attribute.'
+          message:
+            '<style> tags without attributes are not allowed. Allowed: module'
         }
       ],
-      options: ['module']
+      options: [{ allows: ['module'] }]
     },
     {
       code: `<template></template><script></script><style scoped></style>`,
       errors: [
         {
-          message: 'The <style> tag should have the module attribute.'
+          message: 'The scoped attribute is not allowed. Allowed: module'
         }
       ],
-      options: ['module']
+      options: [{ allows: ['module'] }]
+    },
+    // With different combinations of two options
+    {
+      code: `<template></template><script></script><style></style>`,
+      errors: [
+        {
+          message:
+            '<style> tags without attributes are not allowed. Allowed: module, scoped'
+        }
+      ],
+      options: [{ allows: ['module', 'scoped'] }]
+    },
+    {
+      code: `<template></template><script></script><style module></style>`,
+      errors: [
+        {
+          message:
+            'The module attribute is not allowed. Allowed: no-attributes, scoped'
+        }
+      ],
+      options: [{ allows: ['scoped', 'no-attributes'] }]
+    },
+    {
+      code: `<template></template><script></script><style scoped></style>`,
+      errors: [
+        {
+          message:
+            'The scoped attribute is not allowed. Allowed: module, no-attributes'
+        }
+      ],
+      options: [{ allows: ['module', 'no-attributes'] }]
     }
   ]
 })
