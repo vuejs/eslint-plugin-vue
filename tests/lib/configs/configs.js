4a8a6bed28a17ad5be2a427b1f3eeb9de7a5ce1f
Flo Edelmann <git@flo-edelmann.de>
2023-12-04 20:21:11 +0100

    Fix tests

--- tests/lib/configs/configs.js
+++ tests/lib/configs/configs.js
@@ -5,6 +5,10 @@ const plugin = require('../../../lib/index')
 
 describe('configs', () => {
   for (const name of Object.keys(plugin.configs)) {
+    if (name.startsWith('flat/')) {
+      continue
+    }
+
     const configName = `plugin:vue/${name}`
     const eslint = new ESLint({
       overrideConfig: {
