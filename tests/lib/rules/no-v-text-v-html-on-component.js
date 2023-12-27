0ac61d98e956c5f107347c6bf91994da3e9a51cd
Wayne Zhang <zhanghaowei19W@gmail.com>
2023-12-27 15:16:49 +0800

    feat(no-v-text-v-html-on-component): add `allow` option (#2339)
    Co-authored-by: Flo Edelmann <git@flo-edelmann.de>

--- tests/lib/rules/no-v-text-v-html-on-component.js
+++ tests/lib/rules/no-v-text-v-html-on-component.js
@@ -40,6 +40,25 @@ tester.run('no-v-text-v-html-on-component', rule, {
         <MyComponent v-if="content" />
       </template>
       `
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link v-text="content" />
+      </template>
+      `,
+      options: [{ allow: ['router-link'] }]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link v-html="content" />
+        <NuxtLink v-html="content" />
+      </template>
+      `,
+      options: [{ allow: ['RouterLink', 'nuxt-link'] }]
     }
   ],
   invalid: [
@@ -130,6 +149,22 @@ tester.run('no-v-text-v-html-on-component', rule, {
           message: "Using v-html on component may break component's content.",
           line: 3,
           column: 30
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: `
+      <template>
+        <router-link v-html="content" />
+      </template>
+      `,
+      options: [{ allow: ['nuxt-link'] }],
+      errors: [
+        {
+          message: "Using v-html on component may break component's content.",
+          line: 3,
+          column: 22
         }
       ]
     }
