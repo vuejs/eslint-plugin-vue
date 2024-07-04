f5cb93db8fd9ff40288406b6119e3c171b1ce66b
Flo Edelmann <git@flo-edelmann.de>
2024-07-04 13:16:28 +0200

    Improve docs (#2497)

--- docs/.vitepress/build-system/src/eslint.mjs
+++ docs/.vitepress/build-system/src/eslint.mjs
@@ -1,5 +1,8 @@
 // @ts-nocheck
-import * as all from '../../../../node_modules/eslint/lib/linter/linter.js'
-const Linter = all.Linter
-export { Linter }
-export default { Linter }
+/* eslint-disable unicorn/prefer-export-from -- exporting as named and default is less duplication without `export…from` */
+
+import { Linter } from '../../../../node_modules/eslint/lib/linter/linter.js'
+import SourceCode from '../../../../node_modules/eslint/lib/source-code/source-code.js'
+
+export { Linter, SourceCode }
+export default { Linter, SourceCode }
--- docs/user-guide/index.md
+++ docs/user-guide/index.md
@@ -1,7 +1,3 @@
----
-outline: deep
----
-
 # User Guide
 
 ## :cd: Installation
