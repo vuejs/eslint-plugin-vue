a4e2a2ea73e5578560dcb4dd418b7cb9b99d1116
Thomasan1999 <ahoj@tomaskudlac.sk>
2024-11-16 12:03:47 +0100

    fix(prefer-use-template-ref): should check only root-level variables

--- tests/lib/rules/prefer-use-template-ref.js
+++ tests/lib/rules/prefer-use-template-ref.js
@@ -197,6 +197,61 @@ tester.run('prefer-use-template-ref', rule, {
         const button = ref();
         </script>
       `
+    },
+    {
+      filename: 'ref-in-block.vue',
+      code: `
+      <template>
+          <div>
+            <ul>
+              <li ref="firstListItem">Morning</li>
+              <li ref="second">Afternoon</li>
+              <li>Evening</li>
+            </ul>
+          </div>
+      </template>
+      <script setup>
+        import { ref } from 'vue';
+        function getFirstListItemElement() {
+          const firstListItem = ref();
+          const nestedCallback = () => {
+            const second = ref();
+            console.log(second);
+          }
+          nestedCallback();
+        }
+      </script>
+      `
+    },
+    {
+      filename: 'ref-in-block-setup-fn.vue',
+      code: `
+      <template>
+          <div>
+            <ul>
+              <li ref="firstListItem">Morning</li>
+              <li ref="second">Afternoon</li>
+              <li>Evening</li>
+            </ul>
+          </div>
+      </template>
+      <script>
+        import { ref } from 'vue';
+        export default {
+          name: 'ComponentWithRefInBlock',
+          setup() {
+            function getFirstListItemElement() {
+              const firstListItem = ref();
+              const nestedCallback = () => {
+                const second = ref();
+                console.log(second);
+              }
+              nestedCallback();
+            }
+          }
+        }
+      </script>
+      `
     }
   ],
   invalid: [
