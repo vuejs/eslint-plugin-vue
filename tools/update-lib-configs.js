e9e1133510737e91d9a58ba9541afb7eb2a9602d
yosuke ota <otameshiyo23@gmail.com>
2025-01-29 19:56:48 +0900

    fix config

--- tools/update-lib-configs.js
+++ tools/update-lib-configs.js
@@ -58,7 +58,7 @@ function formatCategory(category) {
 module.exports = {
   parser: require.resolve('vue-eslint-parser'),
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: "latest",
     sourceType: 'module'
   },
   env: {
