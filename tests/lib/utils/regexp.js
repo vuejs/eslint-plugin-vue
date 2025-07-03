dd474bb5ac773dcf25cbec27a99c41311c841cf2
ST-DDT <ST-DDT@gmx.de>
2025-07-03 22:56:55 +0200

    refactor: replace regexp loops with regexpGroupMatchers

--- tests/lib/utils/regexp.js
+++ tests/lib/utils/regexp.js
@@ -39,6 +39,68 @@ describe('toRegExp()', () => {
     assert.deepEqual(toRegExp(`${/^bar/i}`), /^bar/i)
     assert.deepEqual(toRegExp(`${/[\sA-Z]+/u}`), /[\sA-Z]+/u)
   })
+
+  it('should handle simple patterns', () => {
+    const regex = toRegExp('foo')
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), false)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), false)
+  })
+
+  it('should handle simple patterns with added flags', () => {
+    const regex = toRegExp('foo', { add: 'i' })
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), false)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), true)
+  })
+
+  it('should handle regexp patterns', () => {
+    const regex = toRegExp('/^foo/')
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), true)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), false)
+  })
+
+  it('should handle regexp patterns with attached flags', () => {
+    const regex = toRegExp('/^foo/i')
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), true)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), true)
+  })
+
+  it('should handle regexp patterns with added flags', () => {
+    const regex = toRegExp('/^foo/', { add: 'i' })
+    assert.deepEqual(regex, /^foo/i)
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), true)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), true)
+  })
+
+  it('should handle regexp patterns with removed flags', () => {
+    const regex = toRegExp('/^foo/i', { remove: 'i' })
+    assert.deepEqual(regex, /^foo/)
+    assert.strictEqual(regex.test('foo'), true)
+    assert.strictEqual(regex.test('bar'), false)
+    assert.strictEqual(regex.test('foobar'), true)
+    assert.strictEqual(regex.test('afoo'), false)
+    assert.strictEqual(regex.test('afoobar'), false)
+    assert.strictEqual(regex.test('Foo'), false)
+  })
 })
 
 describe('toRegExpCheckGroup()', () => {
@@ -76,7 +138,7 @@ describe('toRegExpCheckGroup()', () => {
   })
 
   it('should return a function for single regexp pattern', () => {
-    const groupMatcher = toRegExpGroupMatcher(['/^foo/'])
+    const groupMatcher = toRegExpGroupMatcher(['/^foo/g'])
     assert.strictEqual(groupMatcher(''), false)
     assert.strictEqual(groupMatcher('foo'), true)
     assert.strictEqual(groupMatcher('fooa', 'early'), true)
@@ -86,9 +148,9 @@ describe('toRegExpCheckGroup()', () => {
   })
 
   it('should return a function for multiple regexp patterns', () => {
-    const groupMatcher = toRegExpGroupMatcher(['/^foo/', '/bar$/'])
+    const groupMatcher = toRegExpGroupMatcher(['/^foo/', '/bar$/gi'])
     assert.strictEqual(groupMatcher('foo'), true)
-    assert.strictEqual(groupMatcher('bar', 'early'), true)
+    assert.strictEqual(groupMatcher('Bar', 'early'), true)
     assert.strictEqual(groupMatcher('late', 'matches', 'foo'), true)
     assert.strictEqual(groupMatcher('barfoo'), false)
     assert.strictEqual(groupMatcher('afoo', 'afooa', 'bara'), false)
