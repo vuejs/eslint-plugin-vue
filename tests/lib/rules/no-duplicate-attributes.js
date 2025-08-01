ee3955e60ba41f9666e6e165051c1027c2883af6
ST-DDT <ST-DDT@gmx.de>
2025-08-01 09:17:28 +0200

    test(no-duplicate-attributes): make tests more strict (#2888)

--- tests/lib/rules/no-duplicate-attributes.js
+++ tests/lib/rules/no-duplicate-attributes.js
@@ -62,36 +62,84 @@ tester.run('no-duplicate-attributes', rule, {
     {
       filename: 'test.vue',
       code: '<template><div><div foo v-bind:foo></div></div></template>',
-      errors: ["Duplicate attribute 'foo'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'foo'.",
+          line: 1,
+          column: 25,
+          endLine: 1,
+          endColumn: 35
+        }
+      ]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div foo :foo></div></div></template>',
-      errors: ["Duplicate attribute 'foo'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'foo'.",
+          line: 1,
+          column: 25,
+          endLine: 1,
+          endColumn: 29
+        }
+      ]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div style :style></div></div></template>',
       options: [{ allowCoexistStyle: false }],
-      errors: ["Duplicate attribute 'style'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'style'.",
+          line: 1,
+          column: 27,
+          endLine: 1,
+          endColumn: 33
+        }
+      ]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div class :class></div></div></template>',
       options: [{ allowCoexistClass: false }],
-      errors: ["Duplicate attribute 'class'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'class'.",
+          line: 1,
+          column: 27,
+          endLine: 1,
+          endColumn: 33
+        }
+      ]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div :style style></div></div></template>',
       options: [{ allowCoexistStyle: false }],
-      errors: ["Duplicate attribute 'style'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'style'.",
+          line: 1,
+          column: 28,
+          endLine: 1,
+          endColumn: 33
+        }
+      ]
     },
     {
       filename: 'test.vue',
       code: '<template><div><div :class class></div></div></template>',
       options: [{ allowCoexistClass: false }],
-      errors: ["Duplicate attribute 'class'."]
+      errors: [
+        {
+          message: "Duplicate attribute 'class'.",
+          line: 1,
+          column: 28,
+          endLine: 1,
+          endColumn: 33
+        }
+      ]
     }
   ]
 })
