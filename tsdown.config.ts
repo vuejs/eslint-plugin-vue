7e482cb887682170adca845554d69bc8a8765aed
Vida Xie <vida_2020@163.com>
2025-12-05 11:35:57 +0800

    fix: includes `removed-rules`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['lib/index.ts'],
   format: ['cjs'],
-  copy: ['lib/index.d.ts', 'lib/eslint-typegen.d.ts'],
+  copy: ['lib/index.d.ts', 'lib/eslint-typegen.d.ts', 'lib/removed-rules.js'],
   dts: false,
   external: ['typescript'],
   unbundle: true,
